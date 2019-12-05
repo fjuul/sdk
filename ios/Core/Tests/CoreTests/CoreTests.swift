bd41ba9e5c58cd5476b6936dd47759a5335fbf78
Leo Schweizer <leonhard.schweizer@d-labs.com>
2019-12-05 15:23:28 +0100

    …

--- ios/Core/Tests/CoreTests/CoreTests.swift
+++ ios/Core/Tests/CoreTests/CoreTests.swift
@@ -6,7 +6,7 @@ final class CoreTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Core().text, "Hello, World!")
+        XCTAssertEqual(Core().text, "Hello, Core!")
     }
 
     static var allTests = [

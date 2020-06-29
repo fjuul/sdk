5477c7809dc38a841d90c4c87c5e1fdf330baf19
Dmitry Ovcharov <xesoda@2glab.com>
2020-06-29 14:07:47 +0300

    Refactoring: extract RequestSigner from SigningInterceptor

---
+++ android/core/src/main/java/com/fjuul/sdk/http/utils/RequestSigner.java
@@ -0,0 +1,103 @@
+package com.fjuul.sdk.http.utils;
+
+import com.fjuul.sdk.entities.SigningKey;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+public class RequestSigner {
+    public Request signRequestByKey(Request request, SigningKey key) {
+        Request.Builder signedRequestBuilder = request.newBuilder();
+        String checkingRequestHeaders = this.isRequestWithDigestChecking(request) ?
+            "(request-target) date digest" :
+            "(request-target) date";
+        String requestTargetPart = String.format(
+            "(request-target): %s %s",
+            request.method().toLowerCase(),
+            request.url().encodedPath()
+        );
+        LocalDate date = LocalDate.now();
+        String formattedDate = date.format(DateTimeFormatter.RFC_1123_DATE_TIME);
+        // TODO: assign date format to headers (check if retrofit do it by default) ?
+        String datePart = String.format("date: %s", formattedDate);
+        StringBuilder signingStringBuilder = new StringBuilder(
+            String.format("%s\n%s", requestTargetPart, datePart)
+        );
+        if (isRequestWithDigestChecking(request)) {
+            RequestBody body = request.body();
+            if (body == null) {
+                signingStringBuilder.append("\ndigest: ");
+                signedRequestBuilder.addHeader("Digest", "");
+            } else {
+                String bodyDigest = this.buildDigestOfBody(body);
+                String headerValue = String.format("SHA-256=%s", bodyDigest);
+                String digestPart = String.format("\ndigest: %s", headerValue);
+                signingStringBuilder.append(digestPart);
+                signedRequestBuilder.addHeader("Digest", headerValue);
+            }
+        }
+        String signingString = signingStringBuilder.toString();
+        String signature = buildEncodedEncryptedSignature(signingString, key.getSecret());
+        String signatureHeader = String.format(
+            "keyId=\"%s\",algorithm=\"hmac-sha256\",headers=\"%s\",signature=\"%s\"",
+            key.getId(),
+            checkingRequestHeaders,
+            signature
+        );
+        signedRequestBuilder.addHeader("Signature", signatureHeader);
+        return signedRequestBuilder.build();
+    }
+
+    private String buildDigestOfBody(RequestBody body) {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex);
+        }
+        byte[] stringBodyBytes = body.toString().getBytes(StandardCharsets.UTF_8);
+        byte[] hashedBodyBytes = digest.digest(stringBodyBytes);
+        String encodedDigest = Base64.getEncoder().encodeToString(hashedBodyBytes);
+        return encodedDigest;
+    }
+
+    private Boolean isRequestWithDigestChecking(Request request) {
+        switch (request.method()) {
+            case "PUT":
+            case "POST":
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private String buildEncodedEncryptedSignature(String string, String key) {
+        try {
+            Mac sha256HMAC = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
+            sha256HMAC.init(secretKeySpec);
+            final byte[] signatureBytes = sha256HMAC.doFinal(string.getBytes(StandardCharsets.UTF_8));
+            final Base64.Encoder encoder = Base64.getEncoder();
+            final String result = new String(encoder.encode(signatureBytes), "UTF-8");
+            return  result;
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+        } catch (InvalidKeyException e) {
+            throw new IllegalArgumentException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+}

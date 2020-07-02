f6bf2f6dd5ff3076542e50357bbeba429530d099
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-02 11:57:16 +0300

    Declare interface AnalyticsApi for the analytics package

---
+++ android/analytics/src/main/java/com/fjuul/sdk/analytics/http/AnalyticsApi.java
@@ -0,0 +1,16 @@
+package com.fjuul.sdk.analytics.http;
+
+import com.fjuul.sdk.analytics.entities.DailyStats;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface AnalyticsApi {
+    @GET("analytics/v1/daily-stats/{userToken}/{date}")
+    Call<DailyStats> getDailyStats(@Path("userToken") String userToken);
+
+    @GET("analytics/v1/daily-stats/{userToken}")
+    Call<DailyStats> getDailyStats(@Path("userToken") String userToken, @Query("startDate") String startDate, @Query("endDate") String endDate);
+}

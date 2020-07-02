9f39048744022ebc5d3533e568f007d193a3da9d
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-02 14:08:03 +0300

    Add user-auth-interceptor

---
+++ android/core/src/main/java/com/fjuul/sdk/http/interceptors/UserAuthInterceptor.java
@@ -0,0 +1,26 @@
+package com.fjuul.sdk.http.interceptors;
+
+import com.fjuul.sdk.entities.UserCredentials;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class UserAuthInterceptor implements Interceptor {
+    UserCredentials credentials;
+
+    public UserAuthInterceptor(UserCredentials credentials) {
+        this.credentials = credentials;
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request.Builder builder = chain.request().newBuilder();
+        Request newRequest = builder
+            .header("Authorization", credentials.getCompleteAuthString())
+            .build();
+        return chain.proceed(newRequest);
+    }
+}

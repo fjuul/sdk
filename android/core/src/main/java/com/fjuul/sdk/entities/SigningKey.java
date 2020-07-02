a46f6df6b6bb431d60ddad05f11b80585f927f3b
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-02 19:52:02 +0300

    Remove identityType

--- android/core/src/main/java/com/fjuul/sdk/entities/SigningKey.java
+++ android/core/src/main/java/com/fjuul/sdk/entities/SigningKey.java
@@ -6,22 +6,19 @@ public class SigningKey {
     private String id;
     private String secret;
     private Date expiresAt;
-    // TODO: add enum for identityType
-    private String identityType;
 
     private Boolean valid;
 
     public SigningKey(
-            String id, String secret, Date expiresAt, String identityType, Boolean valid) {
+            String id, String secret, Date expiresAt, Boolean valid) {
         this.id = id;
         this.secret = secret;
         this.expiresAt = expiresAt;
-        this.identityType = identityType;
         this.valid = valid;
     }
 
     public SigningKey(String id, String secret, Date expiresAt, String identityType) {
-        this(id, secret, expiresAt, identityType, true);
+        this(id, secret, expiresAt, true);
     }
 
     public String getId() {
@@ -34,10 +31,6 @@ public class SigningKey {
 
     public Date getExpiresAt() {
         return expiresAt;
-    }
-
-    public String getIdentityType() {
-        return identityType;
     }
 
     public boolean isValid() {

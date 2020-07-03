fe60b0675bc778dcfdf09d139708d52bd704ca1a
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-03 11:17:48 +0300

    Fix: correct an url prefix for the analytics api

--- android/analytics/src/main/java/com/fjuul/sdk/analytics/http/apis/AnalyticsApi.java
+++ android/analytics/src/main/java/com/fjuul/sdk/analytics/http/apis/AnalyticsApi.java
@@ -1,4 +1,4 @@
-package com.fjuul.sdk.analytics.http;
+package com.fjuul.sdk.analytics.http.apis;
 
 import com.fjuul.sdk.analytics.entities.DailyStats;
 
@@ -8,9 +8,9 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface AnalyticsApi {
-    @GET("/analytics/v1/daily-stats/{userToken}/{date}")
+    @GET("/sdk/analytics/v1/daily-stats/{userToken}/{date}")
     Call<DailyStats> getDailyStats(@Path("userToken") String userToken);
 
-    @GET("/analytics/v1/daily-stats/{userToken}")
+    @GET("/sdk/analytics/v1/daily-stats/{userToken}")
     Call<DailyStats> getDailyStats(@Path("userToken") String userToken, @Query("startDate") String startDate, @Query("endDate") String endDate);
 }

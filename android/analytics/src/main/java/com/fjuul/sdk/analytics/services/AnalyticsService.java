5137a37603c6ceaa4456f81014026c87ce78693e
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-03 11:46:12 +0300

    Add AnalyticsService for the analytics module

---
+++ android/analytics/src/main/java/com/fjuul/sdk/analytics/services/AnalyticsService.java
@@ -0,0 +1,40 @@
+package com.fjuul.sdk.analytics.services;
+
+import com.fjuul.sdk.analytics.entities.DailyStats;
+import com.fjuul.sdk.analytics.http.apis.AnalyticsApi;
+import com.fjuul.sdk.entities.SigningKeychain;
+import com.fjuul.sdk.http.FjuulSDKApiHttpClientBuilder;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter;
+
+import java.io.IOException;
+import java.util.Date;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+public class AnalyticsService {
+    private AnalyticsApi analyticsApiClient;
+
+    public AnalyticsService(FjuulSDKApiHttpClientBuilder clientBuilder, SigningKeychain keychain) {
+        OkHttpClient httpClient = clientBuilder.buildSigningClient(keychain);
+        Moshi moshi = new Moshi.Builder().add(Date.class, new Rfc3339DateJsonAdapter()).build();
+        Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(clientBuilder.getBaseUrl())
+            .client(httpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build();
+        analyticsApiClient = retrofit.create(AnalyticsApi.class);
+    }
+
+    public Call<DailyStats> getDailyStats(String userToken, String date) throws IOException {
+        return analyticsApiClient.getDailyStats(userToken, date);
+    }
+
+    public Call<DailyStats> getDailyStats(String userToken, String startDate, String endDate) throws IOException {
+        return analyticsApiClient.getDailyStats(userToken, startDate, endDate);
+    }
+}
+

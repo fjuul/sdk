f96e581e1cd42f987d6326c17f70e08060cdc4be
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-03 11:28:17 +0300

    Fix: add missing path parameter

--- android/analytics/src/main/java/com/fjuul/sdk/analytics/http/apis/AnalyticsApi.java
+++ android/analytics/src/main/java/com/fjuul/sdk/analytics/http/apis/AnalyticsApi.java
@@ -9,7 +9,7 @@ import retrofit2.http.Query;
 
 public interface AnalyticsApi {
     @GET("/sdk/analytics/v1/daily-stats/{userToken}/{date}")
-    Call<DailyStats> getDailyStats(@Path("userToken") String userToken);
+    Call<DailyStats> getDailyStats(@Path("userToken") String userToken, @Path("date") String date);
 
     @GET("/sdk/analytics/v1/daily-stats/{userToken}")
     Call<DailyStats> getDailyStats(@Path("userToken") String userToken, @Query("startDate") String startDate, @Query("endDate") String endDate);

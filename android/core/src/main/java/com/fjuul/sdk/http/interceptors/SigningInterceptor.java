236e6ff8b50fdd5b8be234dc634a6cc8b948e125
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-06 13:55:43 +0300

    Try to retrieve a signing key in SigningInterceptor if no any valid ones

--- android/core/src/main/java/com/fjuul/sdk/http/interceptors/SigningInterceptor.java
+++ android/core/src/main/java/com/fjuul/sdk/http/interceptors/SigningInterceptor.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 
 import com.fjuul.sdk.entities.SigningKey;
 import com.fjuul.sdk.entities.SigningKeychain;
+import com.fjuul.sdk.http.services.ISigningService;
 import com.fjuul.sdk.http.utils.RequestSigner;
 
+import io.reactivex.Maybe;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -14,10 +16,12 @@ import okhttp3.Response;
 public class SigningInterceptor implements Interceptor {
     private SigningKeychain keychain;
     private RequestSigner requestSigner;
+    private ISigningService signingService;
 
-    public SigningInterceptor(SigningKeychain keychain, RequestSigner requestSigner) {
+    public SigningInterceptor(SigningKeychain keychain, RequestSigner requestSigner, ISigningService signingService) {
         this.keychain = keychain;
         this.requestSigner = requestSigner;
+        this.signingService = signingService;
     }
 
     @Override
@@ -26,11 +30,39 @@ public class SigningInterceptor implements Interceptor {
         // TODO: handle case when current valid signing key was expired already
         // TODO: handle case when current valid signing key was rejected by the back-end side
         Optional<SigningKey> keyOptional = this.keychain.getFirstValid();
+        SigningKey signingKey = null;
         if (!keyOptional.isPresent()) {
-            throw new IllegalStateException("No any valid keys to sign the request");
+            // TODO: log about it!
+            SigningKey newKey = issueNewKey();
+            if (newKey != null) {
+                 keychain.appendKey(newKey);
+                 signingKey = newKey;
+            } else {
+                throw new IOException("Couldn't retrieve a signing key");
+            }
+        } else {
+            signingKey = keyOptional.get();
         }
-        SigningKey key = keyOptional.get();
-        Request signedRequest = requestSigner.signRequestByKey(chain.request(), key);
+
+        Request signedRequest = requestSigner.signRequestByKey(chain.request(), signingKey);
         return chain.proceed(signedRequest);
+    }
+
+    private SigningKey issueNewKey() throws IOException {
+        SigningKey newKey = signingService.issueKey()
+            .firstOrError()
+            .flatMapMaybe(signingKeyResult -> {
+                if (signingKeyResult.isError()) {
+                    return Maybe.error(signingKeyResult.error());
+                }
+
+                retrofit2.Response<SigningKey> signingKeyResponse = signingKeyResult.response();
+                if (signingKeyResponse.isSuccessful()) {
+                    return Maybe.just(signingKeyResponse.body());
+                } else {
+                    return Maybe.empty();
+                }
+            }).blockingGet();
+        return newKey;
     }
 }

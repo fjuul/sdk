c235e7f9c9a17a6bef748ab8881c1946ac9e989c
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-06 14:54:39 +0300

    Try to refresh a signing key if get response with 401 status code and error_code is 'expired_signing_key'

--- android/core/src/main/java/com/fjuul/sdk/http/interceptors/SigningInterceptor.java
+++ android/core/src/main/java/com/fjuul/sdk/http/interceptors/SigningInterceptor.java
@@ -5,13 +5,17 @@ import java.util.Optional;
 
 import com.fjuul.sdk.entities.SigningKey;
 import com.fjuul.sdk.entities.SigningKeychain;
+import com.fjuul.sdk.http.responses.UnauthorizedErrorResponseBody;
 import com.fjuul.sdk.http.services.ISigningService;
 import com.fjuul.sdk.http.utils.RequestSigner;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
 
 import io.reactivex.Maybe;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class SigningInterceptor implements Interceptor {
     private SigningKeychain keychain;
@@ -35,6 +39,7 @@ public class SigningInterceptor implements Interceptor {
                  keychain.appendKey(newKey);
                  signingKey = newKey;
             } else {
+                // TODO: specify an error cause (invalid credentials ?)
                 throw new IOException("Couldn't retrieve a signing key");
             }
         } else {
@@ -42,8 +47,31 @@ public class SigningInterceptor implements Interceptor {
         }
 
         Request signedRequest = requestSigner.signRequestByKey(chain.request(), signingKey);
-        // TODO: handle case when current valid signing key was rejected by the back-end side
-        return chain.proceed(signedRequest);
+        Response response = chain.proceed(signedRequest);
+        if (response.isSuccessful()) {
+            return response;
+        }
+
+        // NOTE: try to retrieve a signing key once if error_code is `expired_signing_key`
+        if (response.code() == 401) {
+            ResponseBody responseBody = response.body();
+            Moshi moshi = new Moshi.Builder().build();
+            JsonAdapter<UnauthorizedErrorResponseBody> bodyJsonAdapter = moshi.adapter(UnauthorizedErrorResponseBody.class).nullSafe();
+            UnauthorizedErrorResponseBody errorResponseBody = bodyJsonAdapter.fromJson(responseBody.source());
+            if (errorResponseBody != null && errorResponseBody.getErrorCode() == UnauthorizedErrorResponseBody.ErrorCode.expired_signing_key) {
+                SigningKey newKey = issueNewKey();
+                if (newKey != null) {
+                    keychain.appendKey(newKey);
+                    signingKey = newKey;
+                    signedRequest = requestSigner.signRequestByKey(chain.request(), signingKey);
+                    return chain.proceed(signedRequest);
+                } else {
+                    // TODO: specify an error cause (invalid credentials ?)
+                    throw new IOException("Couldn't refresh a signing key");
+                }
+            }
+        }
+        return response;
     }
 
     private SigningKey issueNewKey() throws IOException {

cb1c543c1ea256df9a09045461282cea9f0e2e28
Leo Schweizer <leo@fjuul.com>
2020-07-07 12:58:00 +0200

    added more signing tests

--- ios/Core/Tests/URLRequest+SigningTests.swift
+++ ios/Core/Tests/URLRequest+SigningTests.swift
@@ -25,4 +25,39 @@ final class URLRequestSiginigTests: XCTestCase {
 
     }
 
+    func testPostRequestWithoutBody() throws {
+
+        var request = try URLRequest(url: "https://fjuul.dev.api/analytics/v1/dailyStats/userToken/2020-01-15", method: .post)
+        request.signWith(key: signingKey, forDate: fixedDate)
+
+        XCTAssertEqual(
+            request.value(forHTTPHeaderField: "Signature"),
+            "keyId=\"signing-key-id-1234\",algorithm=\"hmac-sha256\",headers=\"(request-target) date digest\",signature=\"Mmyp9dkZcBG/7Bk3okAExqvKS/E7bAOyanfAbrdAUnA=\""
+        )
+
+        XCTAssertEqual(request.value(forHTTPHeaderField: "Date"), "Thu, 13 Feb 2020 15:56:23 GMT")
+        XCTAssertEqual(request.value(forHTTPHeaderField: "Digest"), "")
+
+    }
+
+    func testPostRequestWithJsonBody() throws {
+
+        var request = try URLRequest(url: "https://fjuul.dev.api/analytics/v1/dailyStats/userToken/2020-01-15", method: .post)
+        request.httpBody = "{\"hello\":\"world\",\"foo\":\"bar\"}".data(using: String.Encoding.utf8)
+        request.signWith(key: signingKey, forDate: fixedDate)
+
+        XCTAssertEqual(
+            request.value(forHTTPHeaderField: "Signature"),
+            "keyId=\"signing-key-id-1234\",algorithm=\"hmac-sha256\",headers=\"(request-target) date digest\",signature=\"78ygswe54lAGd24/ksNjNXuZ9JNrMTI4E9TsqHaLjaU=\""
+        )
+
+        XCTAssertEqual(request.value(forHTTPHeaderField: "Date"), "Thu, 13 Feb 2020 15:56:23 GMT")
+
+        XCTAssertEqual(
+            request.value(forHTTPHeaderField: "Digest"),
+            "SHA-256=Q95/OQtk+2T6qHbUBZyTr/JITn+2qDMFeqAKJee0Uz0="
+        )
+
+    }
+
 }

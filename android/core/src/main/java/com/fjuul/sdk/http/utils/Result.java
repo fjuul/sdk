1a39661a64438d03da727f152da46c8d8f8d745e
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-13 11:27:59 +0300

    Fix: rename 'success' to 'value'

--- android/core/src/main/java/com/fjuul/sdk/http/utils/Result.java
+++ android/core/src/main/java/com/fjuul/sdk/http/utils/Result.java
@@ -1,22 +1,26 @@
 package com.fjuul.sdk.http.utils;
 
 public final class Result<T, E extends Error> {
-    public static <T,E extends Error> Result<T,E> success(T success) {
-        return new Result<T, E>(success, null);
+    public static <T,E extends Error> Result<T,E> value(T value) {
+        return new Result<T, E>(value, null);
     }
     public static <T,E extends Error> Result<T,E> error(E error) {
         return new Result<>(null, error);
     }
-    private final T success;
+    private final T value;
     private final E error;
 
-    private Result(T success, E error) {
-        this.success = success;
+    private Result(T value, E error) {
+        this.value = value;
         this.error = error;
     }
 
-    public E error() {
+    public E getError() {
         return error;
+    }
+
+    public T getValue() {
+        return value;
     }
 
     public boolean isError() {

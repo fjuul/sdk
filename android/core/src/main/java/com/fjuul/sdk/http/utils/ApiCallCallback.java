90df2e65ff6230662dbeec08d9fb605a31d8207a
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-13 11:27:35 +0300

    Fix: specify return-type for ApiCallCallback

--- android/core/src/main/java/com/fjuul/sdk/http/utils/ApiCallCallback.java
+++ android/core/src/main/java/com/fjuul/sdk/http/utils/ApiCallCallback.java
@@ -1,7 +1,7 @@
 package com.fjuul.sdk.http.utils;
 
-public interface ApiCallCallback<T> {
-    public void onResponse(ApiCall<T> call, T responseBody);
+public interface ApiCallCallback<T, E extends Error, R extends Result<T, E>> {
+    public void onResponse(ApiCall<T> call, R result);
 
     public void onFailure(ApiCall<T> call, Throwable t);
 }

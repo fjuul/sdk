8f9d85ee3f457e59504a4cdc15281c75a704848f
Leo Schweizer <leo@fjuul.com>
2020-07-14 15:13:13 +0200

    fixed naming

--- ios/Core/Tests/HmacAuthenticatiorTests.swift
+++ ios/Core/Tests/HmacAuthenticatiorTests.swift
@@ -14,48 +14,48 @@ final class HmacAuthenticatiorTests: XCTestCase {
     let url = URL(string: "https://foo")!
 
     func testIsAuthenticatedNoCredentialsNoSignature() {
-        let interceptor = HmacAuthenticatior(baseUrl: "https://foo", refreshSession: Session(), credentialStore: store)
+        let authenticator = HmacAuthenticatior(baseUrl: "https://foo", refreshSession: Session(), credentialStore: store)
         let request = URLRequest(url: url)
-        XCTAssertFalse(interceptor.isRequest(request, authenticatedWith: key))
+        XCTAssertFalse(authenticator.isRequest(request, authenticatedWith: key))
     }
 
 
     func testIsAuthenticatedWithCredentialsNoSignature() {
-        let interceptor = HmacAuthenticatior(baseUrl: "https://foo", refreshSession: Session(), credentialStore: store)
+        let authenticator = HmacAuthenticatior(baseUrl: "https://foo", refreshSession: Session(), credentialStore: store)
         let request = URLRequest(url: url)
-        XCTAssertFalse(interceptor.isRequest(request, authenticatedWith: key))
+        XCTAssertFalse(authenticator.isRequest(request, authenticatedWith: key))
     }
 
 
     func testIsAuthenticatedWithCredentialsInvalidKey() {
-        let interceptor = HmacAuthenticatior(baseUrl: "https://foo", refreshSession: Session(), credentialStore: store)
+        let authenticator = HmacAuthenticatior(baseUrl: "https://foo", refreshSession: Session(), credentialStore: store)
         var request = URLRequest(url: url)
         request.addValue(
             "keyId=\"other-key-id\",algorithm=\"hmac-sha256\",headers=\"(request-target) date\",signature=\"ops3L4iYL3YI1mrQ6HJbrNPuYL7av1lkGHluPuuaZig=\"",
             forHTTPHeaderField: "Signature"
         )
-        XCTAssertFalse(interceptor.isRequest(request, authenticatedWith: key))
+        XCTAssertFalse(authenticator.isRequest(request, authenticatedWith: key))
     }
 
 
     func testIsAuthenticatedWithCredentialsWrongKey() {
-        let interceptor = HmacAuthenticatior(baseUrl: "https://foo", refreshSession: Session(), credentialStore: store)
+        let authenticator = HmacAuthenticatior(baseUrl: "https://foo", refreshSession: Session(), credentialStore: store)
         var request = URLRequest(url: url)
         request.addValue(
             "keyId=\"7ac24e53-95a4-479f-a9c5-96c778d5df1d\",algorithm=\"hmac-sha256\",headers=\"(request-target) date\",signature=\"ops3L4iYL3YI1mrQ6HJbrNPuYL7av1lkGHluPuuaZig=\"",
             forHTTPHeaderField: "Signature"
         )
-        XCTAssertFalse(interceptor.isRequest(request, authenticatedWith: key))
+        XCTAssertFalse(authenticator.isRequest(request, authenticatedWith: key))
     }
 
     func testIsAuthenticatedWithCredentialsSameKey() {
-        let interceptor = HmacAuthenticatior(baseUrl: "https://foo", refreshSession: Session(), credentialStore: store)
+        let authenticator = HmacAuthenticatior(baseUrl: "https://foo", refreshSession: Session(), credentialStore: store)
         var request = URLRequest(url: url)
         request.addValue(
             "keyId=\"0b12b123-38f2-410c-b770-82a4f8bc6001\",algorithm=\"hmac-sha256\",headers=\"(request-target) date\",signature=\"ops3L4iYL3YI1mrQ6HJbrNPuYL7av1lkGHluPuuaZig=\"",
             forHTTPHeaderField: "Signature"
         )
-        XCTAssertTrue(interceptor.isRequest(request, authenticatedWith: key))
+        XCTAssertTrue(authenticator.isRequest(request, authenticatedWith: key))
     }
 
 }

700e8409820c5a66ea699d4a8b62fdfee61997d7
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-15 12:04:02 +0300

    Fix: update ApiCall to use particular HttpErrors.CommonError. Catch unauthorized error with code

--- android/core/src/main/java/com/fjuul/sdk/http/utils/ApiCall.java
+++ android/core/src/main/java/com/fjuul/sdk/http/utils/ApiCall.java
@@ -1,6 +1,12 @@
 package com.fjuul.sdk.http.utils;
 
+import com.fjuul.sdk.http.errors.HttpErrors;
+import com.fjuul.sdk.http.responses.ErrorJSONBodyResponse;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 import okhttp3.Request;
 import okio.Timeout;
@@ -21,12 +27,12 @@ public class ApiCall<T> {
         this.delegate = delegate;
     }
 
-    public Result<T, Error> execute() throws IOException {
+    public Result<T, HttpErrors.CommonError> execute() throws IOException {
         Response<T> response = delegate.execute();
         return convertResponseToResult(response);
     }
 
-    public void enqueue(ApiCallCallback<T, Error, Result<T, Error>> callback) {
+    public void enqueue(ApiCallCallback<T, HttpErrors.CommonError, Result<T, HttpErrors.CommonError>> callback) {
         delegate.enqueue(new Callback<T>() {
             @Override
             public void onResponse(Call<T> call, Response<T> response) {
@@ -64,11 +70,33 @@ public class ApiCall<T> {
         return delegate.timeout();
     }
 
-    protected Result<T, Error> convertResponseToResult(Response<T> response) {
+    protected Result<T, HttpErrors.CommonError> convertResponseToResult(Response<T> response) {
         if (response.isSuccessful()) {
             return Result.value(response.body());
         }
+        ErrorJSONBodyResponse responseBody;
+        Moshi moshi = new Moshi.Builder().build();
+        try {
+            JsonAdapter<ErrorJSONBodyResponse> jsonAdapter = moshi.adapter(ErrorJSONBodyResponse.class);
+            responseBody = jsonAdapter.fromJson(response.errorBody().source());
+        } catch (IOException exc) {
+            responseBody = null;
+        }
+
+        String errorMessage = responseBody != null && responseBody.getMessage() != null ? responseBody.getMessage() : "Unknown Error";
+        HttpErrors.CommonError error;
+        if (response.code() == HttpURLConnection.HTTP_UNAUTHORIZED) {
+            HttpErrors.UnauthorizedError.ErrorCode code;
+            try {
+                code = HttpErrors.UnauthorizedError.ErrorCode.valueOf(response.headers().get("x-authentication-error"));
+            } catch (IllegalArgumentException exc) {
+                code = null;
+            }
+            error = new HttpErrors.UnauthorizedError(errorMessage, code);
+        } else {
+            error = new HttpErrors.CommonError(errorMessage);
+        }
         // TODO: add additional checks
-        return Result.error(new Error("Unsuccessful request"));
+        return Result.error(error);
     }
 }

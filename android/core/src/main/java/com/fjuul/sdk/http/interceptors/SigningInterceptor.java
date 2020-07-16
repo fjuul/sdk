e4e1b05ace39fb7a6d2bc371fde5edd55ebf8033
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-16 19:32:30 +0300

    Update SignignInterceptor to implement Authenticator interface

--- android/core/src/main/java/com/fjuul/sdk/http/interceptors/SigningInterceptor.java
+++ android/core/src/main/java/com/fjuul/sdk/http/interceptors/SigningInterceptor.java
@@ -3,20 +3,25 @@ package com.fjuul.sdk.http.interceptors;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.fjuul.sdk.entities.SigningKey;
 import com.fjuul.sdk.entities.SigningKeychain;
 import com.fjuul.sdk.http.services.ISigningService;
 import com.fjuul.sdk.http.utils.RequestSigner;
 
+import okhttp3.Authenticator;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.Route;
 
-public class SigningInterceptor implements Interceptor {
+public class SigningInterceptor implements Interceptor, Authenticator {
     private SigningKeychain keychain;
     private RequestSigner requestSigner;
     private ISigningService signingService;
+    private final Pattern signatureHeaderKeyIdPattern = Pattern.compile("keyId=\"(.+)\"");
 
     public SigningInterceptor(
             SigningKeychain keychain, RequestSigner requestSigner, ISigningService signingService) {
@@ -31,43 +36,70 @@ public class SigningInterceptor implements Interceptor {
         Optional<SigningKey> keyOptional = this.keychain.getFirstValid();
         SigningKey signingKey = null;
         if (!keyOptional.isPresent()) {
-            retrofit2.Response<SigningKey> newKeyResponse = issueNewKey();
-            if (newKeyResponse.isSuccessful()) {
-                SigningKey newKey = newKeyResponse.body();
-                keychain.appendKey(newKey);
-                signingKey = newKey;
-            } else {
-                // return response of a request of signing key to infer http error
-                return extractOriginalRawResponse(newKeyResponse);
+            // need to request a new signing key
+            synchronized (this) {
+                System.out.println("was here by" + Thread.currentThread().getName());
+                // double check if we have now new signing key
+                keyOptional = this.keychain.getFirstValid();
+                // if still no valid key => request new one
+                if (!keyOptional.isPresent()) {
+                    retrofit2.Response<SigningKey> newKeyResponse = issueNewKey();
+                    if (newKeyResponse.isSuccessful()) {
+                        SigningKey newKey = newKeyResponse.body();
+                        keychain.appendKey(newKey);
+                        signingKey = newKey;
+                    } else {
+                        // return response of a request of the signing key to infer http error
+                        return extractOriginalRawResponse(newKeyResponse);
+                    }
+                } else {
+                    signingKey = keyOptional.get();
+                }
             }
         } else {
             signingKey = keyOptional.get();
         }
-
         Request signedRequest = requestSigner.signRequestByKey(chain.request(), signingKey);
-        Response response = chain.proceed(signedRequest);
-        if (response.isSuccessful() || response.code() != 401) {
-            return response;
+        return chain.proceed(signedRequest);
+    }
+
+    @Override
+    public Request authenticate(Route route, Response response) throws IOException {
+        // give up if retry count is more than 1
+        if (responseCount(response) > 1) {
+            return null;
         }
 
         // NOTE: try to retrieve a signing key once if error_code is `expired_signing_key` or
         // 'invalid_key_id'
         String authenticationErrorCode = response.header("x-authentication-error");
-        if (authenticationErrorCode != null
-                && Arrays.asList("invalid_key_id", "expired_signing_key")
-                        .contains(authenticationErrorCode)) {
+        if (!Arrays.asList("invalid_key_id", "expired_signing_key")
+                .contains(authenticationErrorCode)) {
+            return null;
+        }
+
+        synchronized (this) {
+            System.out.println("was here by" + Thread.currentThread().getName());
+            // NOTE: double check if we have now new signing key
+            // (it could be possible since this code block is synchronized)
+            Optional<SigningKey> keyOptional = this.keychain.getFirstValid();
+            Matcher matcher = signatureHeaderKeyIdPattern.matcher(response.header("Signature"));
+            matcher.find();
+            String keyIdMatch = matcher.group(1);
+            if (keyOptional.isPresent() && !keyOptional.get().getId().equals(keyIdMatch)) {
+                SigningKey key = keyOptional.get();
+                return requestSigner.signRequestByKey(response.request(), key);
+            }
+            // if still no valid key => request new one
             retrofit2.Response<SigningKey> newKeyResponse = issueNewKey();
             if (newKeyResponse.isSuccessful()) {
                 SigningKey newKey = newKeyResponse.body();
                 keychain.appendKey(newKey);
-                signedRequest = requestSigner.signRequestByKey(chain.request(), newKey);
-                return chain.proceed(signedRequest);
+                return requestSigner.signRequestByKey(response.request(), newKey);
             } else {
-                // return response of a request of signing key to infer http error
-                return extractOriginalRawResponse(newKeyResponse);
+                return null;
             }
         }
-        return response;
     }
 
     private retrofit2.Response<SigningKey> issueNewKey() throws IOException {
@@ -76,5 +108,13 @@ public class SigningInterceptor implements Interceptor {
 
     private <T> Response extractOriginalRawResponse(retrofit2.Response<T> response) {
         return response.raw().newBuilder().body(response.errorBody()).build();
+    }
+
+    private int responseCount(Response response) {
+        int result = 1;
+        while ((response = response.priorResponse()) != null) {
+            result++;
+        }
+        return result;
     }
 }

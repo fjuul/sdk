842a7b15203462b99cbd4a8851f4fe2ffca70245
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-21 13:47:16 +0300

    Fix ui test

--- android/ExampleApp/app/src/androidTest/java/com/fjuul/sdk/android/ExampleApp/ExampleUiTest.java
+++ android/ExampleApp/app/src/androidTest/java/com/fjuul/sdk/android/ExampleApp/ExampleUiTest.java
@@ -22,6 +22,6 @@ public class ExampleUiTest {
 
     @Test
     public void assertSDKText_mainActivity() {
-        onView(withId(R.id.main_text)).check(matches(withText("Hello, Core!")));
+        onView(withId(R.id.main_text)).check(matches(withText("FJUUL_SDK")));
     }
 }

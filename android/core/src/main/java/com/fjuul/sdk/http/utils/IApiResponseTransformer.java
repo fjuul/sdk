0a6ebefc069615e77340b7b728f626233785583f
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-21 13:15:47 +0300

    Describe ApiResponseTransformer

--- android/core/src/main/java/com/fjuul/sdk/http/utils/IApiResponseTransformer.java
+++ android/core/src/main/java/com/fjuul/sdk/http/utils/IApiResponseTransformer.java
@@ -2,6 +2,16 @@ package com.fjuul.sdk.http.utils;
 
 import retrofit2.Response;
 
+/**
+ * An interface transforms an original retrofit response into ApiCallResult.
+ *
+ * Various services could require to handle an api response differently than usual.
+ * An implementation of this interface solves this problem.
+ *
+ * @see DefaultApiResponseTransformer
+ *
+ * @param <T> Type parameter of casted response type by retrofit converters
+ */
 public interface IApiResponseTransformer<T> {
     public ApiCallResult<T> transform(Response<T> response);
 }

2d074a352253869651153497e44a6f59410a5e87
Leo Schweizer <leo@fjuul.com>
2020-07-24 14:52:23 +0200

    harden date formatters against unexpected user local settings

--- ios/Core/Sources/Utils/DateFormatters.swift
+++ ios/Core/Sources/Utils/DateFormatters.swift
@@ -13,14 +13,21 @@ public struct DateFormatters {
 
     public static let rfc1123: DateFormatter = {
         let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
         formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
         formatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss z"
         return formatter
     }()
 
-    public static let yyyyMMdd: DateFormatter = {
+    public static let yyyyMMddLocale: DateFormatter = {
         let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        // Note: we explicitly use this formatter to format dates without time information
+        // from and to the users local timezone, e.g. to request data for the current local
+        // day of the user, or for handling birthdates
+        formatter.timeZone = TimeZone.current
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter
     }()

d9598998504cd03b2c038d1855a4a54225aaac15
Leo Schweizer <leo@fjuul.com>
2020-07-27 15:32:46 +0200

    deduplicated user profile view logic

--- ios/ExampleApp/ExampleApp/Screens/CreateUserView.swift
+++ ios/ExampleApp/ExampleApp/Screens/CreateUserView.swift
@@ -6,36 +6,21 @@ struct CreateUserView: View {
 
     @Environment(\.presentationMode) var presentation
     @EnvironmentObject var userDefaultsManager: UserDefaultsManager
-
-    @State private var birthDate = Date(timeIntervalSince1970: 0)
-    @State private var height = 170
-    @State private var weight = 80
-    @State private var gender = Gender.other
+    @ObservedObject var userProfile = UserProfileObservable()
 
     var body: some View {
 
         Form {
             Section {
-                DatePicker(selection: $birthDate, displayedComponents: .date, label: { Text("Birthdate") })
-                Stepper(value: $height, in: 100...250, step: 5) {
-                    Text("Height: \(height)cm")
-                }
-                Stepper(value: $weight, in: 35...250, step: 5) {
-                    Text("Weight: \(weight)kg")
-                }
-                Picker(selection: $gender, label: Text("Gender"), content: {
-                    Text("male").tag(Gender.male)
-                    Text("female").tag(Gender.female)
-                    Text("other").tag(Gender.other)
-                })
+                UserProfileForm(showOptionalFields: false).environmentObject(userProfile)
             }
             Section {
                 Button("Create and apply") {
                     let profile = PartialUserProfile([
-                        \UserProfile.birthDate: self.birthDate,
-                        \UserProfile.gender: self.gender,
-                        \UserProfile.height: self.height,
-                        \UserProfile.weight: self.weight
+                        \UserProfile.birthDate: self.userProfile.birthDate,
+                        \UserProfile.gender: self.userProfile.gender,
+                        \UserProfile.height: self.userProfile.height,
+                        \UserProfile.weight: self.userProfile.weight
                     ])
                     ApiClient.createUser(baseUrl: self.userDefaultsManager.environment.baseUrl, apiKey: self.userDefaultsManager.apiKey, profile: profile) { result in
                         switch result {

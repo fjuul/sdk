776bd7710dc982292efd560b8f72012fb6374343
Dmitry Ovcharov <xesoda@2glab.com>
2020-07-27 12:14:18 +0300

    Refactoring: rename storage class to 'PersistentStorage'

--- android/core/src/main/java/com/fjuul/sdk/entities/PersistentStorage.java
+++ android/core/src/main/java/com/fjuul/sdk/entities/PersistentStorage.java
@@ -3,13 +3,13 @@ package com.fjuul.sdk.entities;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-public class PersistentMemoryStorage implements IStorage {
+public class PersistentStorage implements IStorage {
     // TODO: consider moving this string to the resources
     static final String PREFERENCES_NAME = "com.fjuul.sdk.persistence";
 
     SharedPreferences preferences;
 
-    public PersistentMemoryStorage(Context context) {
+    public PersistentStorage(Context context) {
         this.preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
     }
 

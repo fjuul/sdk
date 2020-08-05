fe64a0fca5a69ed10c7b48d61e7956c1ff7d30f9
Dmitry Ovcharov <xesoda@2glab.com>
2020-08-05 12:48:14 +0300

    Add modules list view with sections to ModulesFragment

--- android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/modules/ModulesFragment.kt
+++ android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/modules/ModulesFragment.kt
@@ -1,13 +1,20 @@
 package com.fjuul.sdk.android.exampleapp.ui.modules
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
 import android.widget.ListView
+import android.widget.TextView
 import com.fjuul.sdk.android.exampleapp.R
+
+sealed class ModulesListItem()
+data class ModuleItem(val name: String) : ModulesListItem()
+data class ModulesSection(val name: String) : ModulesListItem()
 
 class ModulesFragment : Fragment() {
     private lateinit var modulesListView: ListView
@@ -27,10 +34,20 @@ class ModulesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         modulesListView = view.findViewById<ListView>(R.id.modules_list)
-        val listItems = listOf<String>("user", "analytics")
-        val adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_list_item_1, listItems)
+        val listItems = arrayListOf<ModulesListItem>(
+            ModulesSection("User"),
+            ModuleItem("Profile"),
+            ModulesSection("Analytics"),
+            ModuleItem("Daily stats"))
+        val adapter = ModulesListAdapter(requireContext(), listItems)
         modulesListView.adapter = adapter
-
+        modulesListView.setOnItemClickListener { parent, view, position, id ->
+            val pressedItem = adapter.getItem(position)
+            if (pressedItem is ModuleItem && pressedItem.name == "DailyStats") {
+                // navigate to analytics fragment
+                Log.d(TAG, "onViewCreated: navigate to analytics")
+            }
+        }
     }
 
     companion object {
@@ -39,5 +56,40 @@ class ModulesFragment : Fragment() {
             ModulesFragment().apply {
                 arguments = Bundle()
             }
+        private const val TAG = "MODULES_FRAGMENT"
     }
+}
+
+class ModulesListAdapter(private val context: Context,
+                         private val dataSource: ArrayList<ModulesListItem>) : BaseAdapter() {
+    private val inflater = LayoutInflater.from(context)
+
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+
+    override fun getItem(position: Int): ModulesListItem {
+        return dataSource[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        // Get view for row item
+        val item = getItem(position)
+        var rowView: View;
+        if (item is ModuleItem) {
+            rowView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+            val textView = rowView.findViewById<TextView>(android.R.id.text1)
+            textView.text = item.name
+        } else {
+            rowView = inflater.inflate(R.layout.section_list_item, parent, false)
+            val textView = rowView.findViewById<TextView>(R.id.section_text)
+            textView.text = (item as ModulesSection).name
+        }
+        return rowView
+    }
+
 }

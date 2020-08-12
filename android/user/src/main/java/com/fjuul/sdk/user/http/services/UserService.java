fe7024f64689077352efd6411f7d2b4c71c0cf06
Dmitry Ovcharov <xesoda@2glab.com>
2020-08-12 16:25:21 +0300

    Remove unused imports

--- android/user/src/main/java/com/fjuul/sdk/user/http/services/UserService.java
+++ android/user/src/main/java/com/fjuul/sdk/user/http/services/UserService.java
@@ -1,7 +1,5 @@
 package com.fjuul.sdk.user.http.services;
 
-import android.os.LocaleList;
-
 import androidx.annotation.NonNull;
 import androidx.core.os.LocaleListCompat;
 
@@ -17,7 +15,6 @@ import com.squareup.moshi.Moshi;
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter;
 
 import java.util.Date;
-import java.util.Locale;
 import java.util.TimeZone;
 
 import okhttp3.OkHttpClient;

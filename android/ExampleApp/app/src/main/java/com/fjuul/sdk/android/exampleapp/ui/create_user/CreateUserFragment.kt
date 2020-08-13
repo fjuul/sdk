7d2876b80a532a33886bfb25b1a876342247e699
Dmitry Ovcharov <xesoda@2glab.com>
2020-08-13 12:38:12 +0300

    Add initial layout for CreateUserFragment

--- android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/create_user/CreateUserFragment.kt
+++ android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/create_user/CreateUserFragment.kt
@@ -1,15 +1,24 @@
 package com.fjuul.sdk.android.exampleapp.ui.create_user
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.fjuul.sdk.android.exampleapp.R
 
 class CreateUserFragment : Fragment() {
     val args: CreateUserFragmentArgs by navArgs()
+    private lateinit var genderDropdown: AutoCompleteTextView
+    private lateinit var birthdateInput: View
+    private lateinit var birthdateText: TextView
+    private lateinit var heightInput: EditText
+    private lateinit var weightInput: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +34,20 @@ class CreateUserFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.dropdown_menu_popup_item,
+            GENDERS
+        )
+        genderDropdown = view.findViewById(R.id.gender_filled_exposed_dropdown)
+        genderDropdown.keyListener = null
+        genderDropdown.setAdapter(adapter)
+
+        birthdateInput = view.findViewById(R.id.birthdate_block)
+        birthdateText = view.findViewById(R.id.birthdate_input)
+        heightInput = view.findViewById(R.id.height_input)
+        weightInput = view.findViewById(R.id.weight_input)
     }
 
     companion object {
@@ -33,5 +56,7 @@ class CreateUserFragment : Fragment() {
             CreateUserFragment().apply {
                 arguments = Bundle()
             }
+
+        val GENDERS = arrayOf("MALE", "FEMALE", "OTHER")
     }
 }

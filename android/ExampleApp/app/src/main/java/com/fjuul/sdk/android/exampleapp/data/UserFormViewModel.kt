eaa67cf7e0150b5bf3215c5a531f73c41c5823f6
Dmitry Ovcharov <xesoda@2glab.com>
2020-08-13 15:42:13 +0300

    Add intial implementation of CreateUserFragment

---
+++ android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/data/UserFormViewModel.kt
@@ -0,0 +1,68 @@
+package com.fjuul.sdk.android.exampleapp.data
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.fjuul.sdk.http.ApiClient
+import com.fjuul.sdk.http.utils.ApiCall
+import com.fjuul.sdk.user.entities.Gender
+import com.fjuul.sdk.user.entities.UserCreationResult
+import com.fjuul.sdk.user.entities.UserProfile
+import com.fjuul.sdk.user.http.services.UserService
+import java.lang.Error
+import java.time.LocalDate
+
+class UserFormViewModel: ViewModel() {
+    private var _profileBuilder: UserProfile.PartialBuilder? = null
+    private val profileBuilder: UserProfile.PartialBuilder
+        private get() {
+            if (_profileBuilder == null) {
+                _profileBuilder = UserProfile.PartialBuilder()
+            }
+            return _profileBuilder!!
+        }
+
+    private val _birthDate = MutableLiveData<LocalDate>(LocalDate.of(1980, 10, 20))
+    val birthDate: LiveData<LocalDate> = _birthDate
+
+    fun setBirthDate(date: LocalDate) {
+        _birthDate.value = date
+        profileBuilder?.setBirthDate(date)
+    }
+
+    private val _height = MutableLiveData<Float>()
+    val height: MutableLiveData<Float> = _height
+
+    fun setHeight(height: Float?) {
+        _height.value = height
+        profileBuilder?.setHeight(height ?: 0f)
+    }
+
+    private val _weight = MutableLiveData<Float>()
+    val weight: MutableLiveData<Float> = _weight
+
+    fun setWeight(weight: Float?) {
+        _weight.value = weight
+        profileBuilder?.setWeight(weight ?: 0f)
+    }
+
+    private val _gender = MutableLiveData<Gender>()
+    private val gender: LiveData<Gender> = _gender
+
+    fun setGender(gender: Gender) {
+        _gender.value = gender
+        profileBuilder?.setGender(gender)
+    }
+
+    @Throws(Error::class)
+    fun createUser(context: Context, apiKey: String): ApiCall<UserCreationResult> {
+        // TODO: pass base url
+        // TODO: thow error if no builder
+        val partialProfile = _profileBuilder ?: throw Error("empty profile params")
+        val client = ApiClient.Builder(context, "https://dev.api.fjuul.com", apiKey).build()
+        return UserService(client).createUser(partialProfile)
+    }
+
+    //TODO: add method to fill live data by user-profile
+}
--- android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/create_user/CreateUserFragment.kt
+++ android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/create_user/CreateUserFragment.kt
@@ -1,24 +1,36 @@
 package com.fjuul.sdk.android.exampleapp.ui.create_user
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.fjuul.sdk.android.exampleapp.R
+import com.fjuul.sdk.android.exampleapp.data.UserFormViewModel
+import com.fjuul.sdk.android.exampleapp.ui.login.afterTextChanged
+import com.fjuul.sdk.user.entities.Gender
+import java.time.LocalDate
 
 class CreateUserFragment : Fragment() {
     val args: CreateUserFragmentArgs by navArgs()
+    private val model: UserFormViewModel by viewModels()
+
     private lateinit var genderDropdown: AutoCompleteTextView
     private lateinit var birthdateInput: View
     private lateinit var birthdateText: TextView
     private lateinit var heightInput: EditText
     private lateinit var weightInput: EditText
+    private lateinit var submitButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +60,59 @@ class CreateUserFragment : Fragment() {
         birthdateText = view.findViewById(R.id.birthdate_input)
         heightInput = view.findViewById(R.id.height_input)
         weightInput = view.findViewById(R.id.weight_input)
+        submitButton = view.findViewById(R.id.create_user_submit_button)
+
+        // birthdate
+        model.birthDate.observe(viewLifecycleOwner, Observer { date ->
+            birthdateText.text = date.toString()
+        })
+        birthdateInput.setOnClickListener {
+            val date = model.birthDate.value!!
+            DatePickerDialog(
+                requireContext(),
+                DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                    model.setBirthDate(LocalDate.of(year, month + 1, dayOfMonth))
+                },
+                date.year,
+                date.monthValue - 1,
+                date.dayOfMonth
+            ).show()
+        }
+
+        // height
+        heightInput.afterTextChanged {
+            model.setHeight(it.toFloatOrNull())
+        }
+
+        // weight
+        weightInput.afterTextChanged {
+            model.setWeight(it.toFloatOrNull())
+        }
+
+        // gender
+        genderDropdown.afterTextChanged {
+            when (it) {
+                GenderPicker.MALE.displayName -> model.setGender(Gender.male)
+                GenderPicker.FEMALE.displayName -> model.setGender(Gender.female)
+                GenderPicker.OTHER.displayName -> model.setGender(Gender.other)
+            }
+        }
+
+        submitButton.setOnClickListener {
+            try {
+                model.createUser(requireContext(), "Zzzz").enqueue { call, result ->
+                    if (result.isError) {
+                        AlertDialog.Builder(requireContext()).setMessage(result.error?.message).show()
+                    }
+                    // fill app storage
+                    //
+                    // result.
+                }
+            } catch (error: Error) {
+                AlertDialog.Builder(requireContext()).setMessage(error.message).show()
+            }
+
+        }
     }
 
     companion object {
@@ -57,6 +122,15 @@ class CreateUserFragment : Fragment() {
                 arguments = Bundle()
             }
 
-        val GENDERS = arrayOf("MALE", "FEMALE", "OTHER")
+        val GENDERS = arrayOf(
+            GenderPicker.MALE.displayName,
+            GenderPicker.FEMALE.displayName,
+            GenderPicker.OTHER.displayName)
     }
+}
+
+public enum class GenderPicker(val displayName: String) {
+    MALE("MALE"),
+    FEMALE("FEMALE"),
+    OTHER("OTHER")
 }

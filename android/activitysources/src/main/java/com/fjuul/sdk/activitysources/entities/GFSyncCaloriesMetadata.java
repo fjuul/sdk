5b3019507b97542b05a2970e7f9dae0fab28e9bf
Dmitry Ovcharov <xesoda@2glab.com>
2020-09-24 13:05:07 +0300

    Add schema-version to GFSyncCaloriesMetadata

--- android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/GFSyncCaloriesMetadata.java
+++ android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/GFSyncCaloriesMetadata.java
@@ -1,27 +1,41 @@
 package com.fjuul.sdk.activitysources.entities;
 
+import androidx.annotation.NonNull;
+
 import java.util.Date;
 
 public class GFSyncCaloriesMetadata {
+    public static final int CURRENT_SCHEMA_VERSION = 1;
+
+    private int schemaVersion;
     private int count;
     private float totalKcals;
     private Date editedAt;
 
     public GFSyncCaloriesMetadata(int count, float totalKcals, Date editedAt) {
+        this.schemaVersion = CURRENT_SCHEMA_VERSION;
         this.count = count;
         this.totalKcals = totalKcals;
         this.editedAt = editedAt;
     }
 
+    @NonNull
     public int getCount() {
         return count;
     }
 
+    @NonNull
     public float getTotalKcals() {
         return totalKcals;
     }
 
+    @NonNull
     public Date getEditedAt() {
         return editedAt;
+    }
+
+    @NonNull
+    public int getSchemaVersion() {
+        return schemaVersion;
     }
 }

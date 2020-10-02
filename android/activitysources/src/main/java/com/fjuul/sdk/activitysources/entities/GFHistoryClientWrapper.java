079c61c0e229c371ce3fed3da91247960131d7e3
Dmitry Ovcharov <xesoda@2glab.com>
2020-10-02 10:26:32 +0300

    [DRAFT] Implement subqueries for google-fit and retries

--- android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/GFHistoryClientWrapper.java
+++ android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/GFHistoryClientWrapper.java
@@ -1,8 +1,10 @@
 package com.fjuul.sdk.activitysources.entities;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.util.Log;
 
+import androidx.arch.core.util.Function;
 import androidx.core.util.Pair;
 
 import com.google.android.gms.fitness.HistoryClient;
@@ -39,10 +41,18 @@ public final class GFHistoryClientWrapper {
         this.gfUtils = gfUtils;
         // TODO: choose the best executor for this wrapper needs (gf data converting)
         // NOTE: the code was taken from https://developers.google.com/android/guides/tasks#threading
-        int numCores = Runtime.getRuntime().availableProcessors();
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(numCores * 2, numCores * 2,
-            60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-        this.executor = executor;
+
+//        this.executor = Executors.newCachedThreadPool();
+//    newFixedThreadPool(50);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            this.executor = Executors.newWorkStealingPool();
+        } else {
+            int numCores = Runtime.getRuntime().availableProcessors();
+            ThreadPoolExecutor executor = new ThreadPoolExecutor(numCores * 2, numCores * 2,
+                60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+            this.executor = executor;
+        }
     }
 
     @SuppressLint("NewApi")
@@ -50,15 +60,55 @@ public final class GFHistoryClientWrapper {
         // NOTE: GF can silently fail on a request if response data is too large,
         // more details at https://stackoverflow.com/a/55806509/6685359
         // TODO: adjust size of chunks (duration) for the best performance
+        Executor gfTaskExecutor = Executors.newFixedThreadPool(5);
         List<Pair<Date, Date>> dateChunks = gfUtils.splitDateRangeIntoChunks(start, end, Duration.ofHours(24));
         List<Task<List<GFCalorieDataPoint>>> tasks = dateChunks.stream().map(dateRange -> {
-            return readCaloriesHistory(dateRange.first, dateRange.second).onSuccessTask(executor, this::convertToCalories);
+            return runReadCaloriesTask(gfTaskExecutor, dateRange);
         }).collect(Collectors.toList());
         Task<List<GFCalorieDataPoint>> getCaloriesTask = Tasks.<List<GFCalorieDataPoint>>whenAllSuccess(tasks).onSuccessTask(executor, lists -> {
             List<GFCalorieDataPoint> flattenList = lists.stream().flatMap(List::stream).collect(Collectors.toList());
             return Tasks.forResult(flattenList);
         });
         return getCaloriesTask;
+    }
+
+    private Task<List<GFCalorieDataPoint>> runReadCaloriesTask(Executor gfTaskExecutor, Pair<Date, Date> dateRange) {
+        return Tasks.call(gfTaskExecutor, () -> {
+            for (int tryNumber = 1; tryNumber <= 5; tryNumber++) {
+                Log.d(TAG, String.format("runReadCaloriesTask: awaiting #%d for %s", tryNumber, dateRange.toString()));
+                try {
+                    Task<List<GFCalorieDataPoint>> originalTask = readCaloriesHistory(dateRange.first, dateRange.second)
+                        .onSuccessTask(executor, this::convertToCalories);
+                    List<GFCalorieDataPoint> result = Tasks.await(originalTask, 60l, TimeUnit.SECONDS);
+                    Log.d(TAG, String.format("runReadCaloriesTask: completed #%d for %s (size: %d)", tryNumber, dateRange.toString(),result.size()));
+                    return result;
+                } catch (
+                    // TODO: catch unauthorized exception
+                    java.util.concurrent.ExecutionException | InterruptedException | java.util.concurrent.TimeoutException exc) {
+                    Log.d(TAG, String.format("runReadCaloriesTask: failed #%d for %s", tryNumber, dateRange.toString()));
+                    continue;
+                }
+            }
+            Log.d(TAG, "runReadCaloriesTask: Too many retries");
+            throw new Exception("Too many retries");
+//            return Tasks.forException(new Exception("Too many retries"));
+        });
+//            .addOnFailureListener(l -> {
+//            Log.d(TAG, "addOnFailureListener: " + l.getMessage());
+//        }).addOnCanceledListener(() -> {
+//            Log.d(TAG, "addOnCanceledListener: ");
+//        })
+//            .continueWithTask(task -> {
+//                if (task.isCanceled() || !task.isSuccessful()) {
+//                    if (task.getException() != null) {
+//                        Log.d(TAG, "Completed due to exception " + task.getException().getMessage());
+//                    }
+//                    Log.d(TAG, "retry for: " + dateRange);
+//                    return runReadCaloriesTask(gfTaskExecutor, dateRange, tryNumber + 1);
+//                }
+//                Log.d(TAG, "finished for: " + dateRange);
+//                return task;
+//        });
     }
 
     private Task<List<GFCalorieDataPoint>> convertToCalories(DataReadResponse dataReadResponse) {

c25b80ef6bf837e28c808f273599715e89faf2ae
Dmitry Ovcharov <xesoda@2glab.com>
2020-10-28 18:51:42 +0300

    Add GFSyncSessionMetadata

---
+++ android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/GFSyncSessionMetadata.java
@@ -0,0 +1,90 @@
+package com.fjuul.sdk.activitysources.entities;
+
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.time.Clock;
+import java.util.Date;
+import java.util.Objects;
+
+class GFSyncSessionMetadata extends GFSyncEntityMetadata {
+    public static final int CURRENT_SCHEMA_VERSION = 1;
+
+    @NonNull private final String id;
+    @NonNull private final String name;
+    @Nullable private final String applicationIdentifier;
+    @NonNull private final Date timeStart;
+    @NonNull private final Date timeEnd;
+    private final int type;
+    private final int activitySegmentsCount;
+    private final int caloriesCount;
+    private final int stepsCount;
+    private final int heartRateCount;
+    private final int speedCount;
+    private final int powerCount;
+
+    public GFSyncSessionMetadata(@NonNull String id, @NonNull String name, @Nullable String applicationIdentifier,
+                                 @NonNull Date timeStart, @NonNull Date timeEnd, int type,
+                                 int activitySegmentsCount, int caloriesCount, int stepsCount,
+                                 int heartRateCount, int speedCount, int powerCount, Date editedAt) {
+        super(CURRENT_SCHEMA_VERSION, editedAt);
+        this.id = id;
+        this.name = name;
+        this.applicationIdentifier = applicationIdentifier;
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+        this.type = type;
+        this.activitySegmentsCount = activitySegmentsCount;
+        this.caloriesCount = caloriesCount;
+        this.stepsCount = stepsCount;
+        this.heartRateCount = heartRateCount;
+        this.speedCount = speedCount;
+        this.powerCount = powerCount;
+    }
+
+    @SuppressLint("NewApi")
+    static public GFSyncSessionMetadata buildFromSessionBundle(GFSessionBundle sessionBundle, Clock clock) {
+        final int activitySegmentsCount = sessionBundle.getActivitySegments().size();
+        final int caloriesCount = sessionBundle.getCalories().size();
+        final int stepsCount = sessionBundle.getSteps().size();
+        final int heartRateCount = sessionBundle.getHeartRate().size();
+        final int speedCount = sessionBundle.getSpeed().size();
+        final int powerCount = sessionBundle.getPower().size();
+        final Date editedAt = Date.from(clock.instant());
+        return new GFSyncSessionMetadata(
+            sessionBundle.getId(),
+            sessionBundle.getName(),
+            sessionBundle.getApplicationIdentifier(),
+            sessionBundle.getTimeStart(),
+            sessionBundle.getTimeEnd(),
+            sessionBundle.getType(),
+            activitySegmentsCount,
+            caloriesCount,
+            stepsCount,
+            heartRateCount,
+            speedCount,
+            powerCount,
+            editedAt);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GFSyncSessionMetadata that = (GFSyncSessionMetadata) o;
+        return type == that.type &&
+            activitySegmentsCount == that.activitySegmentsCount &&
+            caloriesCount == that.caloriesCount &&
+            stepsCount == that.stepsCount &&
+            heartRateCount == that.heartRateCount &&
+            speedCount == that.speedCount &&
+            powerCount == that.powerCount &&
+            id.equals(that.id) &&
+            name.equals(that.name) &&
+            Objects.equals(applicationIdentifier, that.applicationIdentifier) &&
+            timeStart.equals(that.timeStart) &&
+            timeEnd.equals(that.timeEnd);
+    }
+}

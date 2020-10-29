bdb9e6f6e40d444d2721e621c727c2cb589f846f
Dmitry Ovcharov <xesoda@2glab.com>
2020-10-29 18:11:11 +0300

    Refactoring: extract all converting methods from GFClientWrapper to GFDataConverter and make them static

--- android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/GFClientWrapper.java
+++ android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/GFClientWrapper.java
@@ -138,7 +138,7 @@ public final class GFClientWrapper {
     private Task<List<GFCalorieDataPoint>> runReadCaloriesTask(Pair<Date, Date> dateRange, SupervisedExecutor gfTaskWatcher) {
         Supplier<Task<List<GFCalorieDataPoint>>> taskSupplier = () -> {
             return readCaloriesHistory(dateRange.first, dateRange.second)
-                .onSuccessTask(executor, this::convertDataReadResponseToCalories);
+                .onSuccessTask(executor, GFDataConverter::convertDataReadResponseToCalories);
         };
         return runGFTaskUnderWatch(taskSupplier, gfTaskWatcher);
     }
@@ -146,7 +146,7 @@ public final class GFClientWrapper {
     private Task<List<GFStepsDataPoint>> runReadStepsTask(Pair<Date, Date> dateRange, SupervisedExecutor gfTaskWatcher) {
         Supplier<Task<List<GFStepsDataPoint>>> taskSupplier = () -> {
             return readStepsHistory(dateRange.first, dateRange.second)
-                .onSuccessTask(executor, this::convertDataReadResponseToSteps);
+                .onSuccessTask(executor, GFDataConverter::convertDataReadResponseToSteps);
         };
         return runGFTaskUnderWatch(taskSupplier, gfTaskWatcher);
     }
@@ -155,7 +155,7 @@ public final class GFClientWrapper {
     private Task<List<GFHRSummaryDataPoint>> runReadHRTask(Pair<Date, Date> dateRange, Duration bucketDuration, SupervisedExecutor gfTaskWatcher) {
         Supplier<Task<List<GFHRSummaryDataPoint>>> taskSupplier = () -> {
             return readHRHistory(dateRange.first, dateRange.second, bucketDuration)
-                .onSuccessTask(executor, this::convertDataReadResponseToHRSummaries);
+                .onSuccessTask(executor, GFDataConverter::convertDataReadResponseToHRSummaries);
         };
         return runGFTaskUnderWatch(taskSupplier, gfTaskWatcher);
     }
@@ -215,146 +215,6 @@ public final class GFClientWrapper {
             return Tasks.forResult(flattenResults);
         });
         return collectResultsTask;
-    }
-
-    private Task<List<GFCalorieDataPoint>> convertDataReadResponseToCalories(DataReadResponse dataReadResponse) {
-        ArrayList<GFCalorieDataPoint> calorieDataPoints = new ArrayList<>();
-        for (Bucket bucket : dataReadResponse.getBuckets()) {
-            Date start = new Date(bucket.getStartTime(TimeUnit.MILLISECONDS));
-            if (bucket.getDataSets().isEmpty()) {
-                continue;
-            }
-            DataSet dataSet = bucket.getDataSets().get(0);
-            if (dataSet.isEmpty()) {
-                continue;
-            }
-            DataPoint calorieDataPoint = dataSet.getDataPoints().get(0);
-            String dataSourceId = calorieDataPoint.getOriginalDataSource().getStreamIdentifier();
-            for (Field field : DataType.TYPE_CALORIES_EXPENDED.getFields()) {
-                if (Field.FIELD_CALORIES.equals(field)) {
-                    float kcals = calorieDataPoint.getValue(field).asFloat();
-                    GFCalorieDataPoint calorie = new GFCalorieDataPoint(kcals, start, dataSourceId);
-                    calorieDataPoints.add(calorie);
-                }
-            }
-        }
-        return Tasks.forResult(calorieDataPoints);
-    }
-
-    private GFCalorieDataPoint convertDataPointToCalorie(DataPoint dataPoint) {
-        String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
-        Date start = new Date(dataPoint.getStartTime(TimeUnit.MILLISECONDS));
-        Date end = new Date(dataPoint.getEndTime(TimeUnit.MILLISECONDS));
-        for (Field field : DataType.TYPE_CALORIES_EXPENDED.getFields()) {
-            if (Field.FIELD_CALORIES.equals(field)) {
-                float kcals = dataPoint.getValue(field).asFloat();
-                return new GFCalorieDataPoint(kcals, start, end, dataSourceId);
-            }
-        }
-        return null;
-    }
-
-    private Task<List<GFStepsDataPoint>> convertDataReadResponseToSteps(DataReadResponse dataReadResponse) {
-        ArrayList<GFStepsDataPoint> stepsDataPoints = new ArrayList<>();
-        for (Bucket bucket : dataReadResponse.getBuckets()) {
-            Date start = new Date(bucket.getStartTime(TimeUnit.MILLISECONDS));
-            if (bucket.getDataSets().isEmpty()) {
-                continue;
-            }
-            DataSet dataSet = bucket.getDataSets().get(0);
-            if (dataSet.isEmpty()) {
-                continue;
-            }
-            DataPoint stepsDataPoint = dataSet.getDataPoints().get(0);
-            String dataSourceId = stepsDataPoint.getOriginalDataSource().getStreamIdentifier();
-            for (Field field : DataType.TYPE_STEP_COUNT_DELTA.getFields()) {
-                if (Field.FIELD_STEPS.equals(field)) {
-                    int steps = stepsDataPoint.getValue(field).asInt();
-                    GFStepsDataPoint convertedStepsDataPoint = new GFStepsDataPoint(steps, start, dataSourceId);
-                    stepsDataPoints.add(convertedStepsDataPoint);
-                }
-            }
-        }
-        return Tasks.forResult(stepsDataPoints);
-    }
-
-    private GFStepsDataPoint convertDataPointToSteps(DataPoint dataPoint) {
-        Date start = new Date(dataPoint.getStartTime(TimeUnit.MILLISECONDS));
-        Date end = new Date(dataPoint.getEndTime(TimeUnit.MILLISECONDS));
-        String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
-        for (Field field : DataType.TYPE_STEP_COUNT_DELTA.getFields()) {
-            if (Field.FIELD_STEPS.equals(field)) {
-                int steps = dataPoint.getValue(field).asInt();
-                return new GFStepsDataPoint(steps, start, end, dataSourceId);
-            }
-        }
-        return null;
-    }
-
-    private Task<List<GFHRSummaryDataPoint>> convertDataReadResponseToHRSummaries(DataReadResponse dataReadResponse) {
-        ArrayList<GFHRSummaryDataPoint> hrDataPoints = new ArrayList<>();
-        for (Bucket bucket : dataReadResponse.getBuckets()) {
-            Date start = new Date(bucket.getStartTime(TimeUnit.MILLISECONDS));
-            if (bucket.getDataSets().isEmpty()) {
-                continue;
-            }
-            DataSet dataSet = bucket.getDataSets().get(0);
-            if (dataSet.isEmpty()) {
-                continue;
-            }
-            DataPoint dataPoint = dataSet.getDataPoints().get(0);
-            String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
-            float avgBPM = dataPoint.getValue(Field.FIELD_AVERAGE).asFloat();
-            float minBPM = dataPoint.getValue(Field.FIELD_MIN).asFloat();
-            float maxBPM = dataPoint.getValue(Field.FIELD_MAX).asFloat();
-            GFHRSummaryDataPoint hrSummary = new GFHRSummaryDataPoint(avgBPM, minBPM, maxBPM, start, dataSourceId);
-            hrDataPoints.add(hrSummary);
-        }
-        return Tasks.forResult(hrDataPoints);
-    }
-
-    private GFHRDataPoint convertDataPointToHR(DataPoint dataPoint) {
-        String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
-        Date start = new Date(dataPoint.getTimestamp(TimeUnit.MILLISECONDS));
-        for (Field field : DataType.TYPE_HEART_RATE_BPM.getFields()) {
-            if (Field.FIELD_BPM.equals(field)) {
-                float bpm = dataPoint.getValue(field).asFloat();
-                return new GFHRDataPoint(bpm, start, dataSourceId);
-            }
-        }
-        return null;
-    }
-
-    private GFPowerDataPoint convertDataPointToPower(DataPoint dataPoint) {
-        String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
-        Date start = new Date(dataPoint.getTimestamp(TimeUnit.MILLISECONDS));
-        for (Field field : DataType.TYPE_POWER_SAMPLE.getFields()) {
-            if (Field.FIELD_WATTS.equals(field)) {
-                float watts = dataPoint.getValue(field).asFloat();
-                return new GFPowerDataPoint(watts, start, dataSourceId);
-            }
-        }
-        return null;
-    }
-
-    private GFActivitySegmentDataPoint convertDataPointToActivitySegment(DataPoint dataPoint) {
-        final String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
-        final Date start = new Date(dataPoint.getStartTime(TimeUnit.MILLISECONDS));
-        final Date end = new Date(dataPoint.getEndTime(TimeUnit.MILLISECONDS));
-        final int activityType = dataPoint.getValue(Field.FIELD_ACTIVITY).asInt();
-        return new GFActivitySegmentDataPoint(activityType, start,end,dataSourceId);
-    }
-
-    private GFSpeedDataPoint convertDataPointToSpeed(DataPoint dataPoint) {
-        String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
-        Date start = new Date(dataPoint.getTimestamp(TimeUnit.MILLISECONDS));
-        for (Field field : DataType.TYPE_SPEED.getFields()) {
-            if (Field.FIELD_SPEED.equals(field)) {
-                float metersPerSecond = dataPoint.getValue(field).asFloat();
-                return new GFSpeedDataPoint(metersPerSecond, start, dataSourceId);
-            }
-        }
-        return null;
     }
 
     @SuppressLint("NewApi")
@@ -422,22 +282,22 @@ public final class GFClientWrapper {
         for (DataSet dataSet : response.getDataSet(session)) {
             DataType dataType = dataSet.getDataType();
             if (dataType.equals(DataType.TYPE_HEART_RATE_BPM)) {
-                List<GFHRDataPoint> hr = convertDataSetToPoints(dataSet, this::convertDataPointToHR);
+                List<GFHRDataPoint> hr = convertDataSetToPoints(dataSet, GFDataConverter::convertDataPointToHR);
                 sessionBundleBuilder.setHeartRate(hr);
             } else if (dataType.equals(DataType.TYPE_STEP_COUNT_DELTA)) {
-                List<GFStepsDataPoint> steps = convertDataSetToPoints(dataSet, this::convertDataPointToSteps);
+                List<GFStepsDataPoint> steps = convertDataSetToPoints(dataSet, GFDataConverter::convertDataPointToSteps);
                 sessionBundleBuilder.setSteps(steps);
             } else if (dataType.equals(DataType.TYPE_SPEED)) {
-                List<GFSpeedDataPoint> speed = convertDataSetToPoints(dataSet, this::convertDataPointToSpeed);
+                List<GFSpeedDataPoint> speed = convertDataSetToPoints(dataSet, GFDataConverter::convertDataPointToSpeed);
                 sessionBundleBuilder.setSpeed(speed);
             } else if (dataType.equals(DataType.TYPE_POWER_SAMPLE)) {
-                List<GFPowerDataPoint> power = convertDataSetToPoints(dataSet, this::convertDataPointToPower);
+                List<GFPowerDataPoint> power = convertDataSetToPoints(dataSet, GFDataConverter::convertDataPointToPower);
                 sessionBundleBuilder.setPower(power);
             } else if (dataType.equals(DataType.TYPE_CALORIES_EXPENDED)) {
-                List<GFCalorieDataPoint> calories = convertDataSetToPoints(dataSet, this::convertDataPointToCalorie);
+                List<GFCalorieDataPoint> calories = convertDataSetToPoints(dataSet, GFDataConverter::convertDataPointToCalorie);
                 sessionBundleBuilder.setCalories(calories);
             } else if (dataType.equals(DataType.TYPE_ACTIVITY_SEGMENT)) {
-                List<GFActivitySegmentDataPoint> activitySegments = convertDataSetToPoints(dataSet, this::convertDataPointToActivitySegment);
+                List<GFActivitySegmentDataPoint> activitySegments = convertDataSetToPoints(dataSet, GFDataConverter::convertDataPointToActivitySegment);
                 sessionBundleBuilder.setActivitySegments(activitySegments);
             }
         }
---
+++ android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/GFDataConverter.java
@@ -0,0 +1,159 @@
+package com.fjuul.sdk.activitysources.entities;
+
+import android.annotation.SuppressLint;
+
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class GFDataConverter {
+    public static Task<List<GFCalorieDataPoint>> convertDataReadResponseToCalories(DataReadResponse dataReadResponse) {
+        ArrayList<GFCalorieDataPoint> calorieDataPoints = new ArrayList<>();
+        for (Bucket bucket : dataReadResponse.getBuckets()) {
+            Date start = new Date(bucket.getStartTime(TimeUnit.MILLISECONDS));
+            if (bucket.getDataSets().isEmpty()) {
+                continue;
+            }
+            DataSet dataSet = bucket.getDataSets().get(0);
+            if (dataSet.isEmpty()) {
+                continue;
+            }
+            DataPoint calorieDataPoint = dataSet.getDataPoints().get(0);
+            String dataSourceId = calorieDataPoint.getOriginalDataSource().getStreamIdentifier();
+            for (Field field : DataType.TYPE_CALORIES_EXPENDED.getFields()) {
+                if (Field.FIELD_CALORIES.equals(field)) {
+                    float kcals = calorieDataPoint.getValue(field).asFloat();
+                    GFCalorieDataPoint calorie = new GFCalorieDataPoint(kcals, start, dataSourceId);
+                    calorieDataPoints.add(calorie);
+                }
+            }
+        }
+        return Tasks.forResult(calorieDataPoints);
+    }
+
+    public static GFCalorieDataPoint convertDataPointToCalorie(DataPoint dataPoint) {
+        String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
+        Date start = new Date(dataPoint.getStartTime(TimeUnit.MILLISECONDS));
+        Date end = new Date(dataPoint.getEndTime(TimeUnit.MILLISECONDS));
+        for (Field field : DataType.TYPE_CALORIES_EXPENDED.getFields()) {
+            if (Field.FIELD_CALORIES.equals(field)) {
+                float kcals = dataPoint.getValue(field).asFloat();
+                return new GFCalorieDataPoint(kcals, start, end, dataSourceId);
+            }
+        }
+        return null;
+    }
+
+    public static Task<List<GFStepsDataPoint>> convertDataReadResponseToSteps(DataReadResponse dataReadResponse) {
+        ArrayList<GFStepsDataPoint> stepsDataPoints = new ArrayList<>();
+        for (Bucket bucket : dataReadResponse.getBuckets()) {
+            Date start = new Date(bucket.getStartTime(TimeUnit.MILLISECONDS));
+            if (bucket.getDataSets().isEmpty()) {
+                continue;
+            }
+            DataSet dataSet = bucket.getDataSets().get(0);
+            if (dataSet.isEmpty()) {
+                continue;
+            }
+            DataPoint stepsDataPoint = dataSet.getDataPoints().get(0);
+            String dataSourceId = stepsDataPoint.getOriginalDataSource().getStreamIdentifier();
+            for (Field field : DataType.TYPE_STEP_COUNT_DELTA.getFields()) {
+                if (Field.FIELD_STEPS.equals(field)) {
+                    int steps = stepsDataPoint.getValue(field).asInt();
+                    GFStepsDataPoint convertedStepsDataPoint = new GFStepsDataPoint(steps, start, dataSourceId);
+                    stepsDataPoints.add(convertedStepsDataPoint);
+                }
+            }
+        }
+        return Tasks.forResult(stepsDataPoints);
+    }
+
+    public static GFStepsDataPoint convertDataPointToSteps(DataPoint dataPoint) {
+        Date start = new Date(dataPoint.getStartTime(TimeUnit.MILLISECONDS));
+        Date end = new Date(dataPoint.getEndTime(TimeUnit.MILLISECONDS));
+        String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
+        for (Field field : DataType.TYPE_STEP_COUNT_DELTA.getFields()) {
+            if (Field.FIELD_STEPS.equals(field)) {
+                int steps = dataPoint.getValue(field).asInt();
+                return new GFStepsDataPoint(steps, start, end, dataSourceId);
+            }
+        }
+        return null;
+    }
+
+    public static Task<List<GFHRSummaryDataPoint>> convertDataReadResponseToHRSummaries(DataReadResponse dataReadResponse) {
+        ArrayList<GFHRSummaryDataPoint> hrDataPoints = new ArrayList<>();
+        for (Bucket bucket : dataReadResponse.getBuckets()) {
+            Date start = new Date(bucket.getStartTime(TimeUnit.MILLISECONDS));
+            if (bucket.getDataSets().isEmpty()) {
+                continue;
+            }
+            DataSet dataSet = bucket.getDataSets().get(0);
+            if (dataSet.isEmpty()) {
+                continue;
+            }
+            DataPoint dataPoint = dataSet.getDataPoints().get(0);
+            String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
+            float avgBPM = dataPoint.getValue(Field.FIELD_AVERAGE).asFloat();
+            float minBPM = dataPoint.getValue(Field.FIELD_MIN).asFloat();
+            float maxBPM = dataPoint.getValue(Field.FIELD_MAX).asFloat();
+            GFHRSummaryDataPoint hrSummary = new GFHRSummaryDataPoint(avgBPM, minBPM, maxBPM, start, dataSourceId);
+            hrDataPoints.add(hrSummary);
+        }
+        return Tasks.forResult(hrDataPoints);
+    }
+
+    public static GFHRDataPoint convertDataPointToHR(DataPoint dataPoint) {
+        String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
+        Date start = new Date(dataPoint.getTimestamp(TimeUnit.MILLISECONDS));
+        for (Field field : DataType.TYPE_HEART_RATE_BPM.getFields()) {
+            if (Field.FIELD_BPM.equals(field)) {
+                float bpm = dataPoint.getValue(field).asFloat();
+                return new GFHRDataPoint(bpm, start, dataSourceId);
+            }
+        }
+        return null;
+    }
+
+    public static GFPowerDataPoint convertDataPointToPower(DataPoint dataPoint) {
+        String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
+        Date start = new Date(dataPoint.getTimestamp(TimeUnit.MILLISECONDS));
+        for (Field field : DataType.TYPE_POWER_SAMPLE.getFields()) {
+            if (Field.FIELD_WATTS.equals(field)) {
+                float watts = dataPoint.getValue(field).asFloat();
+                return new GFPowerDataPoint(watts, start, dataSourceId);
+            }
+        }
+        return null;
+    }
+
+    public static GFActivitySegmentDataPoint convertDataPointToActivitySegment(DataPoint dataPoint) {
+        final String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
+        final Date start = new Date(dataPoint.getStartTime(TimeUnit.MILLISECONDS));
+        final Date end = new Date(dataPoint.getEndTime(TimeUnit.MILLISECONDS));
+        final int activityType = dataPoint.getValue(Field.FIELD_ACTIVITY).asInt();
+        return new GFActivitySegmentDataPoint(activityType, start,end,dataSourceId);
+    }
+
+    public static GFSpeedDataPoint convertDataPointToSpeed(DataPoint dataPoint) {
+        String dataSourceId = dataPoint.getOriginalDataSource().getStreamIdentifier();
+        Date start = new Date(dataPoint.getTimestamp(TimeUnit.MILLISECONDS));
+        for (Field field : DataType.TYPE_SPEED.getFields()) {
+            if (Field.FIELD_SPEED.equals(field)) {
+                float metersPerSecond = dataPoint.getValue(field).asFloat();
+                return new GFSpeedDataPoint(metersPerSecond, start, dataSourceId);
+            }
+        }
+        return null;
+    }
+}

03b148a5e4cb21fccd2ccbe3153c22ad25bbb899
Dmitry Ovcharov <xesoda@2glab.com>
2020-11-06 18:37:56 +0300

    Fix: add missing ActivitySourceConnection

---
+++ android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/ActivitySourceConnection.java
@@ -0,0 +1,18 @@
+package com.fjuul.sdk.activitysources.entities;
+
+import androidx.annotation.NonNull;
+
+public class ActivitySourceConnection extends TrackerConnection {
+    @NonNull
+    private ActivitySource activitySource;
+
+    public ActivitySourceConnection(@NonNull TrackerConnection trackerConnection, @NonNull ActivitySource activitySource) {
+        super(trackerConnection.getId(), trackerConnection.getTracker(), trackerConnection.getCreatedAt(), trackerConnection.getEndedAt());
+        this.activitySource = activitySource;
+    }
+
+    @NonNull
+    public ActivitySource getActivitySource() {
+        return activitySource;
+    }
+}

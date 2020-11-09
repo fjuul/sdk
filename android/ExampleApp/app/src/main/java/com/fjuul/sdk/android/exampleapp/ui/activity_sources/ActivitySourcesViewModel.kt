bda6e983f28ceda9cdfadaafc31c19bb930ca8bf
Dmitry Ovcharov <xesoda@2glab.com>
2020-11-09 13:09:33 +0300

    Fix: update ActivitySourcesViewModel according to the last state of activity sources module

--- android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/activity_sources/ActivitySourcesViewModel.kt
+++ android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/activity_sources/ActivitySourcesViewModel.kt
@@ -3,29 +3,27 @@ package com.fjuul.sdk.android.exampleapp.ui.activity_sources
 import android.content.Intent
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+
 import androidx.lifecycle.ViewModel
 import com.fjuul.sdk.activitysources.entities.ActivitySource
+import com.fjuul.sdk.activitysources.entities.ActivitySourceConnection
 import com.fjuul.sdk.activitysources.entities.ActivitySourcesManager
-import com.fjuul.sdk.activitysources.entities.TrackerConnection
-import com.fjuul.sdk.activitysources.http.services.ActivitySourcesService
-import com.fjuul.sdk.android.exampleapp.data.model.ApiClientHolder
 
 class ActivitySourcesViewModel() : ViewModel() {
-    private val activitySourcesService = ActivitySourcesService(ApiClientHolder.sdkClient)
-    private val _currentConnections = MutableLiveData<Array<TrackerConnection>>(arrayOf())
+    private val _currentConnections = MutableLiveData<List<ActivitySourceConnection>>(listOf())
     private val _errorMessage = MutableLiveData<String>()
     private val _connectionIntent = MutableLiveData<Pair<ActivitySource, Intent>>()
 
-    val currentConnections: LiveData<Array<TrackerConnection>> = _currentConnections
+    val currentConnections: LiveData<List<ActivitySourceConnection>> = _currentConnections
     val errorMessage: LiveData<String> = _errorMessage
     val connectionIntent: LiveData<Pair<ActivitySource, Intent>?> = _connectionIntent
 
     fun fetchCurrentConnections() {
-        activitySourcesService.currentConnections.enqueue { call, result ->
+        val manager = ActivitySourcesManager.getInstance()
+        manager.refreshCurrent { result ->
             if (result.isError) {
-                _currentConnections.postValue(arrayOf())
                 _errorMessage.postValue(result.error?.message)
-                return@enqueue
+                return@refreshCurrent
             }
             _currentConnections.postValue(result.value)
         }
@@ -50,12 +48,13 @@ class ActivitySourcesViewModel() : ViewModel() {
         }
         // NOTE: currently only one connection can be active
         val connection = connections.first()
-        activitySourcesService.disconnect(connection).enqueue { call, result ->
+        val manager = ActivitySourcesManager.getInstance()
+        manager.disconnect(connection) { result ->
             if (result.isError) {
                 _errorMessage.postValue(result.error?.message)
-                return@enqueue
+                return@disconnect
             }
-            fetchCurrentConnections()
+            _currentConnections.postValue(result.value)
         }
     }
 

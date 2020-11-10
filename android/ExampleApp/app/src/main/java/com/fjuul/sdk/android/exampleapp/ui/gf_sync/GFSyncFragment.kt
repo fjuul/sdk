0082310b0d594a87c85b8c9ae4ce9b6b4c8ad3a1
Dmitry Ovcharov <xesoda@2glab.com>
2020-11-10 10:46:42 +0300

    Implement GFSync screen

--- android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/gf_sync/GFSyncFragment.kt
+++ android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/gf_sync/GFSyncFragment.kt
@@ -1,12 +1,20 @@
 package com.fjuul.sdk.android.exampleapp.ui.gf_sync
 
-import androidx.lifecycle.ViewModelProvider
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.fjuul.sdk.android.exampleapp.R
+import kotlinx.android.synthetic.main.gf_sync_fragment.*
+import java.lang.Exception
+import java.time.Duration
+import java.time.LocalDate
 
 class GFSyncFragment : Fragment() {
 
@@ -26,6 +34,109 @@ class GFSyncFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(GFSyncViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        (intraday_section_text as TextView).text = "Intraday metrics"
+        (sessions_section_text as TextView).text = "Sessions"
+
+        viewModel.startDate.observe(
+            viewLifecycleOwner,
+            Observer { date ->
+                start_date_value_text.text = date.toString()
+            }
+        )
+        viewModel.endDate.observe(
+            viewLifecycleOwner,
+            Observer {
+                end_date_value_text.text = it.toString()
+            }
+        )
+        viewModel.syncingIntradayMetrics.observe(
+            viewLifecycleOwner,
+            Observer { syncing ->
+                intraday_sync_progress_bar.visibility = when (syncing) {
+                    true -> View.VISIBLE
+                    false -> View.INVISIBLE
+                }
+            }
+        )
+
+        viewModel.syncingSessions.observe(
+            viewLifecycleOwner,
+            Observer { syncing ->
+                sessions_sync_progress_bar.visibility = when (syncing) {
+                    true -> View.VISIBLE
+                    false -> View.INVISIBLE
+                }
+            }
+        )
+        viewModel.errorMessage.observe(
+            viewLifecycleOwner,
+            Observer {
+                if (it == null) {
+                    return@Observer
+                }
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Error")
+                    .setMessage(it)
+                    .show()
+                viewModel.resetErrorMessage()
+            }
+        )
+
+        start_date_input_layout.setOnClickListener {
+            // val date = model.startDate.value!!
+            val date = LocalDate.now()
+            DatePickerDialog(
+                requireContext(),
+                DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                    viewModel.setupDateRange(startDate = LocalDate.of(year, month + 1, dayOfMonth))
+                },
+                date.year,
+                date.monthValue - 1,
+                date.dayOfMonth
+            ).show()
+        }
+        end_date_input_layout.setOnClickListener {
+            // val date = model.startDate.value!!
+            val date = LocalDate.now()
+            DatePickerDialog(
+                requireContext(),
+                DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                    viewModel.setupDateRange(startDate = LocalDate.of(year, month + 1, dayOfMonth))
+                },
+                date.year,
+                date.monthValue - 1,
+                date.dayOfMonth
+            ).show()
+        }
+
+
+        min_session_duration_text_edit.onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
+            if (!hasFocus && min_session_duration_text_edit.text.isNullOrEmpty()) {
+                min_session_duration_text_edit.setText("3")
+            }
+        }
+
+        run_intraday_sync_button.setOnClickListener {
+            val calories = calories_check_box.isChecked
+            val steps = steps_check_box.isChecked
+            val heartRate = heart_rate_check_box.isChecked
+            viewModel.runIntradaySync(calories, heartRate, steps)
+        }
+
+        run_sessions_sync_button.setOnClickListener {
+            val minutesDurationText = min_session_duration_text_edit.text.toString()
+            var minSessionDuration: Duration? = null
+            try {
+                val minutes = minutesDurationText.toInt()
+                if (minutes > 0) {
+                    minSessionDuration = Duration.ofMinutes(minutes.toLong())
+                }
+            } catch (exc: Exception) { }
+            if (minSessionDuration == null) {
+                return@setOnClickListener
+            }
+            viewModel.runSessionsSync(minSessionDuration)
+        }
     }
 }
--- android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/gf_sync/GFSyncViewModel.kt
+++ android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/gf_sync/GFSyncViewModel.kt
@@ -1,7 +1,82 @@
 package com.fjuul.sdk.android.exampleapp.ui.gf_sync
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.fjuul.sdk.activitysources.entities.ActivitySourcesManager
+import com.fjuul.sdk.activitysources.entities.GFIntradaySyncOptions
+import com.fjuul.sdk.activitysources.entities.GFSessionSyncOptions
+import com.fjuul.sdk.activitysources.entities.GoogleFitActivitySource
+import java.time.Duration
+import java.time.LocalDate
 
 class GFSyncViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _startDate = MutableLiveData<LocalDate>(LocalDate.now())
+    private val _endDate = MutableLiveData<LocalDate>(LocalDate.now())
+    private val _syncingIntradayMetrics = MutableLiveData<Boolean>(false)
+    private val _syncingSessions = MutableLiveData<Boolean>(false)
+    private val _errorMessage = MutableLiveData<String?>()
+
+    val startDate: LiveData<LocalDate> = _startDate
+    val endDate: LiveData<LocalDate> = _endDate
+    val syncingIntradayMetrics: LiveData<Boolean> = _syncingIntradayMetrics
+    val syncingSessions: LiveData<Boolean> = _syncingSessions
+    val errorMessage: LiveData<String?> = _errorMessage
+
+    fun setupDateRange(startDate: LocalDate? = null, endDate: LocalDate? = null) {
+        if (startDate != null) {
+            _startDate.postValue(startDate)
+        }
+        if (endDate != null) {
+            _endDate.postValue(endDate)
+        }
+    }
+
+    fun resetErrorMessage() {
+        _errorMessage.postValue(null)
+    }
+
+    fun runIntradaySync(calories: Boolean, hr: Boolean, steps: Boolean) {
+        val manager = ActivitySourcesManager.getInstance()
+        val gfConnectionSource = manager.current?.find { connection -> connection.activitySource is GoogleFitActivitySource }
+        if (gfConnectionSource == null) {
+            _errorMessage.value = "No active Google Fit connection"
+            return
+        }
+        val options = GFIntradaySyncOptions.Builder().apply {
+            setDateRange(_startDate.value, _endDate.value)
+            if (calories) { include(GFIntradaySyncOptions.METRICS_TYPE.CALORIES) }
+            if (hr) { include(GFIntradaySyncOptions.METRICS_TYPE.HEART_RATE) }
+            if (steps) { include(GFIntradaySyncOptions.METRICS_TYPE.STEPS) }
+        }.build()
+        _syncingIntradayMetrics.postValue(true)
+        (gfConnectionSource.activitySource as GoogleFitActivitySource).syncIntradayMetrics(options) { result ->
+            _syncingIntradayMetrics.postValue(false)
+            if (result.isError) {
+                _errorMessage.postValue(result.error?.message)
+                return@syncIntradayMetrics
+            }
+        }
+    }
+
+    fun runSessionsSync(minSessionDuration: Duration) {
+        val manager = ActivitySourcesManager.getInstance()
+        val gfConnectionSource = manager.current?.find { connection -> connection.activitySource is GoogleFitActivitySource }
+        if (gfConnectionSource == null) {
+            _errorMessage.value = "No active Google Fit connection"
+            return
+        }
+        val options = GFSessionSyncOptions.Builder()
+            .setDateRange(_startDate.value!!, _endDate.value!!)
+            .setMinimumSessionDuration(minSessionDuration)
+            .build()
+        _syncingSessions.postValue(true)
+        (gfConnectionSource.activitySource as GoogleFitActivitySource).syncSessions(options) { result ->
+            _syncingSessions.postValue(false)
+            if (result.isError) {
+                _errorMessage.postValue(result.error?.message)
+                return@syncSessions
+            }
+        }
+    }
 }

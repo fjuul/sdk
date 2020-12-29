58cf6b423063ac75d0d1ef13a2f3ab7381c99ffb
Dmitry Ovcharov <xesoda@2glab.com>
2020-12-29 12:54:32 +0300

    Add javadoc of ExternalAuthenticationFlowHandler

--- android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/ExternalAuthenticationFlowHandler.java
+++ android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/ExternalAuthenticationFlowHandler.java
@@ -2,13 +2,26 @@ package com.fjuul.sdk.activitysources.entities;
 
 import android.net.Uri;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.Arrays;
+import java.util.Objects;
 
+/**
+ * Handler for the result of connecting to external activity sources. Before calling the {@link #handle(Uri)} method,
+ * you should check that the schema of the incoming intent matches the expected for Fjuul SDK.
+ */
 public final class ExternalAuthenticationFlowHandler {
+    /**
+     * Determines the status of connecting to the external activity source and returns either it if the data of the
+     * intent successfully recognized or null.
+     * @param data data of the incoming intent
+     * @return connection status
+     */
     @Nullable
-    public static ConnectionStatus handle(Uri data) {
+    public static ConnectionStatus handle(@NonNull Uri data) {
+        Objects.requireNonNull(data);
         if (data.getHost() != null && data.getHost().contains("external_connect") &&
             data.getQueryParameterNames().containsAll(Arrays.asList("success", "service"))) {
             final String service = data.getQueryParameter("service");
@@ -19,14 +32,21 @@ public final class ExternalAuthenticationFlowHandler {
     }
 
     public static final class ConnectionStatus {
+        @NonNull
         private final String service;
         private final boolean success;
 
-        protected ConnectionStatus(String service, boolean success) {
+        protected ConnectionStatus(@NonNull String service, boolean success) {
             this.service = service;
             this.success = success;
         }
 
+        /**
+         * Returns a string representation of the external activity source being connected that can be matched with
+         * {@link ActivitySource.TrackerValue#getValue()}.
+         * @return external service
+         */
+        @NonNull
         public String getService() {
             return service;
         }

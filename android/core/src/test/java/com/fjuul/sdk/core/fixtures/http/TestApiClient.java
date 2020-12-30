841ba02af685bea3125636b0098cedfb4b9d6dea
Dmitry Ovcharov <xesoda@2glab.com>
2020-12-30 18:46:25 +0300

    Fix: provide the dummy application context to the constuctor of ApiClient

--- android/core/src/test/java/com/fjuul/sdk/core/fixtures/http/TestApiClient.java
+++ android/core/src/test/java/com/fjuul/sdk/core/fixtures/http/TestApiClient.java
@@ -3,15 +3,20 @@ package com.fjuul.sdk.core.fixtures.http;
 import com.fjuul.sdk.core.ApiClient;
 import com.fjuul.sdk.core.entities.Keystore;
 
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
 import okhttp3.mockwebserver.MockWebServer;
 
 public final class TestApiClient {
     public static final String TEST_API_KEY = "TEST_API_KEY";
 
     public static class Builder extends ApiClient.Builder {
-        // TODO: add constructor overloading for the stubbed application context
         public Builder(MockWebServer mockWebServer) {
-            super(null, mockWebServer.url("/").toString(), TEST_API_KEY);
+            this(ApplicationProvider.getApplicationContext(), mockWebServer);
+        }
+
+        public Builder(Context context, MockWebServer mockWebServer) {
+            super(context, mockWebServer.url("/").toString(), TEST_API_KEY);
         }
 
         public Builder setKeystore(Keystore keystore) {

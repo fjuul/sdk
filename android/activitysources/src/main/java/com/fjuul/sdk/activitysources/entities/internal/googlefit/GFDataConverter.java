4338c513c49b3eb53de41ca513a6ac32e6ae65ed
Hesowcharov <Hesowcharov@gmail.com>
2021-01-25 19:04:30 +0300

    Fix: infer unique datasource of data points (#23)
    * Fix: infer own string presentation of the data source by trimming the steam name
    
    * Fix failing unit tests
    
    * Lint code
    
    * Fix: take the original data source
    
    * Refactoring: rename the method
    
    * Add a comment about the approach
    
    * Add an example describing changes in data source identifiers
    
    Co-authored-by: Dmitry Ovcharov <xesoda@2glab.com>

--- android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/internal/googlefit/GFDataConverter.java
+++ android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/internal/googlefit/GFDataConverter.java
@@ -1,15 +1,20 @@
 package com.fjuul.sdk.activitysources.entities.internal.googlefit;
 
 import java.util.Date;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.google.android.gms.fitness.data.Bucket;
 import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataSource;
 import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Device;
 import com.google.android.gms.fitness.data.Field;
 
+import android.annotation.SuppressLint;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -27,14 +32,14 @@ public class GFDataConverter {
             return null;
         }
         final DataPoint calorieDataPoint = dataSet.getDataPoints().get(0);
-        final String dataSourceId = tryToExtractDataSourceStreamId(calorieDataPoint);
+        final String dataSourceId = tryToExtractDataSourceIdentifier(calorieDataPoint);
         float kcals = calorieDataPoint.getValue(Field.FIELD_CALORIES).asFloat();
         return new GFCalorieDataPoint(kcals, start, dataSourceId);
     }
 
     @NonNull
     public static GFCalorieDataPoint convertDataPointToCalorie(@NonNull DataPoint dataPoint) {
-        final String dataSourceId = tryToExtractDataSourceStreamId(dataPoint);
+        final String dataSourceId = tryToExtractDataSourceIdentifier(dataPoint);
         final Date start = new Date(dataPoint.getStartTime(TimeUnit.MILLISECONDS));
         final Date end = new Date(dataPoint.getEndTime(TimeUnit.MILLISECONDS));
         float kcals = dataPoint.getValue(Field.FIELD_CALORIES).asFloat();
@@ -52,7 +57,7 @@ public class GFDataConverter {
             return null;
         }
         final DataPoint stepsDataPoint = dataSet.getDataPoints().get(0);
-        final String dataSourceId = tryToExtractDataSourceStreamId(stepsDataPoint);
+        final String dataSourceId = tryToExtractDataSourceIdentifier(stepsDataPoint);
         int steps = stepsDataPoint.getValue(Field.FIELD_STEPS).asInt();
         return new GFStepsDataPoint(steps, start, dataSourceId);
     }
@@ -61,7 +66,7 @@ public class GFDataConverter {
     public static GFStepsDataPoint convertDataPointToSteps(@NonNull DataPoint dataPoint) {
         final Date start = new Date(dataPoint.getStartTime(TimeUnit.MILLISECONDS));
         final Date end = new Date(dataPoint.getEndTime(TimeUnit.MILLISECONDS));
-        final String dataSourceId = tryToExtractDataSourceStreamId(dataPoint);
+        final String dataSourceId = tryToExtractDataSourceIdentifier(dataPoint);
         int steps = dataPoint.getValue(Field.FIELD_STEPS).asInt();
         return new GFStepsDataPoint(steps, start, end, dataSourceId);
     }
@@ -77,7 +82,7 @@ public class GFDataConverter {
             return null;
         }
         DataPoint dataPoint = dataSet.getDataPoints().get(0);
-        String dataSourceId = tryToExtractDataSourceStreamId(dataPoint);
+        String dataSourceId = tryToExtractDataSourceIdentifier(dataPoint);
         float avgBPM = dataPoint.getValue(Field.FIELD_AVERAGE).asFloat();
         float minBPM = dataPoint.getValue(Field.FIELD_MIN).asFloat();
         float maxBPM = dataPoint.getValue(Field.FIELD_MAX).asFloat();
@@ -86,7 +91,7 @@ public class GFDataConverter {
 
     @NonNull
     public static GFHRDataPoint convertDataPointToHR(@NonNull DataPoint dataPoint) {
-        String dataSourceId = tryToExtractDataSourceStreamId(dataPoint);
+        String dataSourceId = tryToExtractDataSourceIdentifier(dataPoint);
         Date start = new Date(dataPoint.getTimestamp(TimeUnit.MILLISECONDS));
         float bpm = dataPoint.getValue(Field.FIELD_BPM).asFloat();
         return new GFHRDataPoint(bpm, start, dataSourceId);
@@ -94,7 +99,7 @@ public class GFDataConverter {
 
     @NonNull
     public static GFPowerDataPoint convertDataPointToPower(@NonNull DataPoint dataPoint) {
-        String dataSourceId = tryToExtractDataSourceStreamId(dataPoint);
+        String dataSourceId = tryToExtractDataSourceIdentifier(dataPoint);
         Date start = new Date(dataPoint.getTimestamp(TimeUnit.MILLISECONDS));
         float watts = dataPoint.getValue(Field.FIELD_WATTS).asFloat();
         return new GFPowerDataPoint(watts, start, dataSourceId);
@@ -102,7 +107,7 @@ public class GFDataConverter {
 
     @NonNull
     public static GFActivitySegmentDataPoint convertDataPointToActivitySegment(@NonNull DataPoint dataPoint) {
-        final String dataSourceId = tryToExtractDataSourceStreamId(dataPoint);
+        final String dataSourceId = tryToExtractDataSourceIdentifier(dataPoint);
         final Date start = new Date(dataPoint.getStartTime(TimeUnit.MILLISECONDS));
         final Date end = new Date(dataPoint.getEndTime(TimeUnit.MILLISECONDS));
         final int activityType = dataPoint.getValue(Field.FIELD_ACTIVITY).asInt();
@@ -111,18 +116,72 @@ public class GFDataConverter {
 
     @NonNull
     public static GFSpeedDataPoint convertDataPointToSpeed(@NonNull DataPoint dataPoint) {
-        String dataSourceId = tryToExtractDataSourceStreamId(dataPoint);
+        String dataSourceId = tryToExtractDataSourceIdentifier(dataPoint);
         Date start = new Date(dataPoint.getTimestamp(TimeUnit.MILLISECONDS));
         float metersPerSecond = dataPoint.getValue(Field.FIELD_SPEED).asFloat();
         return new GFSpeedDataPoint(metersPerSecond, start, dataSourceId);
     }
 
+    /**
+     * NOTE: we don't use provided {@code streamIdentifier} and {@code streamName} from DataSource API because in
+     * practice the name of the stream (the last part of {@code streamIdentifier}) may vary which violates the
+     * uniqueness of the common identifier for the same source of data. Own implementation is close to the original one
+     * except ignoring empty parts delimited by ":".<br>
+     * For example, original {@code streamIdentifier}s:
+     *
+     * <pre>
+     * derived:com.google.calories.expended:com.google.android.gms:overlay_explicit_input_local,
+     * raw:com.google.calories.expended:com.mc.miband1:Xiaomi:Mi Smart Band 4:51e74c1f:Notify for Mi Band - calories,
+     * raw:com.google.calories.expended:com.mc.miband1:Notify for Mi Band - workout calories,
+     * derived:com.google.calories.expended:com.google.android.gms:from_activities,
+     * derived:com.google.calories.expended:com.google.android.gms:from_activities_local
+     * </pre>
+     *
+     * will be transformed to:
+     *
+     * <pre>
+     * raw:com.google.calories.expended:com.mc.miband1:Xiaomi:Mi Smart Band 4:51e74c1f,
+     * raw:com.google.calories.expended:com.mc.miband1,
+     * derived:com.google.calories.expended:com.google.android.gms
+     * </pre>
+     */
+    @SuppressLint("NewApi")
     @Nullable
-    private static String tryToExtractDataSourceStreamId(@NonNull DataPoint point) {
+    private static String tryToExtractDataSourceIdentifier(@NonNull DataPoint point) {
         final DataSource dataSource = point.getOriginalDataSource();
         if (dataSource == null) {
             return null;
         }
-        return dataSource.getStreamIdentifier();
+        String type = "unknown";
+        switch (dataSource.getType()) {
+            case DataSource.TYPE_RAW:
+                type = "raw";
+                break;
+            case DataSource.TYPE_DERIVED:
+                type = "derived";
+                break;
+        }
+        final StringBuilder dataSourceIdentifierBuilder = new StringBuilder();
+        dataSourceIdentifierBuilder.append(type).append(":").append(dataSource.getDataType().getName());
+        final String packageName = dataSource.getAppPackageName();
+        if (!isNullOrEmptyString(packageName)) {
+            dataSourceIdentifierBuilder.append(":").append(packageName);
+        }
+        final Device sourceDevice = dataSource.getDevice();
+        if (sourceDevice != null) {
+            final String deviceInfo =
+                Stream.of(sourceDevice.getManufacturer(), sourceDevice.getModel(), sourceDevice.getUid())
+                    .filter(value -> !isNullOrEmptyString(value))
+                    .collect(Collectors.joining(":"));
+            if (!isNullOrEmptyString(deviceInfo)) {
+                dataSourceIdentifierBuilder.append(":").append(deviceInfo);
+            }
+        }
+        return dataSourceIdentifierBuilder.toString();
+    }
+
+    @SuppressLint("NewApi")
+    private static boolean isNullOrEmptyString(@Nullable String string) {
+        return Objects.isNull(string) || string.isEmpty();
     }
 }

f227ec7fe4256a663b396cf61610765416e507e1
Dmitry Ovcharov <xesoda@2glab.com>
2021-01-26 18:09:55 +0300

    Add SDKUserAgentInterceptor

---
+++ android/core/src/main/java/com/fjuul/sdk/core/http/interceptors/SDKUserAgentInterceptor.java
@@ -0,0 +1,29 @@
+package com.fjuul.sdk.core.http.interceptors;
+
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class SDKUserAgentInterceptor implements Interceptor {
+    @NonNull
+    private final String agent;
+
+    public SDKUserAgentInterceptor(@NonNull String versionName) {
+        agent = String.format(Locale.ROOT, "Fjuul Android SDK %s", versionName);
+    }
+
+    @SuppressLint("UnknownNullness")
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        final Request.Builder builder = chain.request().newBuilder();
+        final Request newRequest = builder.header("User-Agent", agent).build();
+        return chain.proceed(newRequest);
+    }
+}

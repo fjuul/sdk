caf146d824c445ffdce4750d61e44357240aad1d
Dmitry Ovcharov <xesoda@2glab.com>
2021-01-27 16:11:21 +0300

    Fix: lint code

--- android/user/src/main/java/com/fjuul/sdk/user/http/responses/ValidationErrorJSONBodyResponse.java
+++ android/user/src/main/java/com/fjuul/sdk/user/http/responses/ValidationErrorJSONBodyResponse.java
@@ -1,13 +1,14 @@
 package com.fjuul.sdk.user.http.responses;
 
-import androidx.annotation.NonNull;
-
 import java.util.List;
 
 import com.fjuul.sdk.core.http.responses.ErrorJSONBodyResponse;
 
+import androidx.annotation.NonNull;
+
 public class ValidationErrorJSONBodyResponse extends ErrorJSONBodyResponse {
-    @NonNull private List<ValidationError> errors;
+    @NonNull
+    private List<ValidationError> errors;
 
     @NonNull
     public List<ValidationError> getErrors() {

256d4f13a838354105fbacf20279bfde9cb748eb
Dmitry Ovcharov <xesoda@2glab.com>
2021-02-09 18:33:39 +0300

    Add initial implementation of FjuulSDKTimberTree (based on DebugTree)

---
+++ android/core/src/main/java/com/fjuul/sdk/core/utils/FjuulSDKTimberTree.java
@@ -0,0 +1,103 @@
+package com.fjuul.sdk.core.utils;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import timber.log.Timber;
+
+public class FjuulSDKTimberTree extends Timber.Tree {
+    private static final int MAX_LOG_LENGTH = 4000;
+    protected static final String TAG = "Fjuul-SDK";
+    protected static final Pattern SDK_MODULE_NAME = Pattern.compile("^com.fjuul.sdk.([^.]+)\\.");
+
+    @Override
+    protected void log(int priority, @Nullable String _tag, @NotNull String message, @Nullable Throwable t) {
+        // TODO: provide our own implementation of log
+        // TODO: allow SDK consumers to override 'log' method with the same formatting and tagging
+        final List<StackTraceElement> filteredTrace = getFilteredStackTraceOfCaller();
+        String messagePrefix = "";
+        if (filteredTrace.size() > 0) {
+            final String callerClassName = inferCallerClassName(filteredTrace.get(0));
+            final String sdkModuleName = inferSdkModuleName(filteredTrace.get(0));
+            final StringBuilder messagePrefixBuilder = new StringBuilder();
+            if (sdkModuleName != null) {
+                messagePrefixBuilder.append(String.format(Locale.ROOT, "[%s]", sdkModuleName));
+            }
+            messagePrefix = messagePrefixBuilder.append(String.format(Locale.ROOT," %s: ", callerClassName)).toString();
+        }
+        message = messagePrefix + message;
+        if (message.length() < MAX_LOG_LENGTH) {
+            if (priority == Log.ASSERT) {
+                Log.wtf(TAG, message);
+            } else {
+                Log.println(priority, TAG, message);
+            }
+            return;
+        }
+
+        // Split by line, then ensure each line can fit into Log's maximum length.
+        for (int i = 0, length = message.length(); i < length; i++) {
+            int newline = message.indexOf('\n', i);
+            newline = newline != -1 ? newline : length;
+            do {
+                int end = Math.min(newline, i + MAX_LOG_LENGTH);
+                String part = message.substring(i, end);
+                if (priority == Log.ASSERT) {
+                    Log.wtf(TAG, part);
+                } else {
+                    Log.println(priority, TAG, part);
+                }
+                i = end;
+            } while (i < newline);
+        }
+    }
+
+    protected List<StackTraceElement> getFilteredStackTraceOfCaller() {
+        final StackTraceElement[] rawStackTrace = new Throwable().getStackTrace();
+        final ArrayList<StackTraceElement> filteredStackTrace = new ArrayList<>();
+        boolean isReachedTimberFromTop = false;
+        boolean isReachedSdkAfterTimber = false;
+        for (final StackTraceElement element : rawStackTrace) {
+            if (!isReachedTimberFromTop) {
+                if (isTimberStackTraceElement(element)) {
+                    isReachedTimberFromTop = true;
+                }
+                continue;
+            }
+            if (!isReachedSdkAfterTimber) {
+                if (isTimberStackTraceElement(element)) {
+                    continue;
+                }
+                isReachedSdkAfterTimber = true;
+            }
+            filteredStackTrace.add(element);
+        }
+        return filteredStackTrace;
+    }
+
+    private boolean isTimberStackTraceElement(@NonNull StackTraceElement element) {
+        return element.getClassName().equals(Timber.class.getName());
+    }
+
+    protected String inferCallerClassName(@NonNull StackTraceElement element) {
+        final String fullClassName = element.getClassName();
+        return fullClassName.substring(fullClassName.lastIndexOf('.') + 1);
+    }
+
+    @Nullable
+    protected String inferSdkModuleName(@NonNull StackTraceElement element) {
+        final String fullClassName = element.getClassName();
+        final Matcher matcher = SDK_MODULE_NAME.matcher(fullClassName);
+        return matcher.find() ? matcher.group(1) : null;
+    }
+}

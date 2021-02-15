4fcdeccd53c0c57668453793d2dd9bc4a8a3a421
Dmitry Ovcharov <xesoda@2glab.com>
2021-02-15 17:31:47 +0300

    Extend FitnessMetricsType by HEIGHT and WEIGHT

--- android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/FitnessMetricsType.java
+++ android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/FitnessMetricsType.java
@@ -3,13 +3,16 @@ package com.fjuul.sdk.activitysources.entities;
 import androidx.annotation.NonNull;
 
 /**
- * General enumeration of activity metrics used in Fjuul SDK.
+ * General enumeration of activity metrics and human's measurements used in Fjuul SDK.
  */
 public enum FitnessMetricsType {
     INTRADAY_CALORIES,
     INTRADAY_HEART_RATE,
     INTRADAY_STEPS,
-    WORKOUTS;
+    WORKOUTS,
+
+    HEIGHT,
+    WEIGHT;
 
     public static boolean isIntradayMetricType(@NonNull FitnessMetricsType metricType) {
         switch (metricType) {

df18f57f9fa616d4ac92261683a1cc2d2fc37c76
Dmitry Ovcharov <xesoda@2glab.com>
2021-02-17 17:20:21 +0300

    Add GFSyncHeightMetadata and GFSyncWeightMetadata

---
+++ android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/internal/googlefit/sync_metadata/GFSyncHeightMetadata.java
@@ -0,0 +1,42 @@
+package com.fjuul.sdk.activitysources.entities.internal.googlefit.sync_metadata;
+
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+
+import com.fjuul.sdk.activitysources.entities.internal.googlefit.GFHeightDataPoint;
+
+import java.time.Clock;
+import java.util.Date;
+
+public class GFSyncHeightMetadata extends GFSyncEntityMetadata {
+    public static final int CURRENT_SCHEMA_VERSION = 1;
+    private static final float HEIGHT_CM_ACCURACY = 0.1f;
+
+    private final float height;
+    @NonNull
+    private final Date createdAt;
+
+    public GFSyncHeightMetadata(float height, @NonNull Date createdAt, @NonNull Date editedAt) {
+        super(CURRENT_SCHEMA_VERSION, editedAt);
+        this.height = height;
+        this.createdAt = createdAt;
+    }
+
+    @SuppressLint("NewApi")
+    static public GFSyncHeightMetadata buildFromDataPoint(@NonNull GFHeightDataPoint dataPoint, @NonNull Clock clock) {
+        float height = dataPoint.getValue();
+        final Date date = dataPoint.getStart();
+        final Date editedAt = Date.from(clock.instant());
+        return new GFSyncHeightMetadata(height, date, editedAt);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final GFSyncHeightMetadata that = (GFSyncHeightMetadata) o;
+        return createdAt.equals(that.createdAt) && Math.abs(height - that.height) <= HEIGHT_CM_ACCURACY;
+    }
+}
+

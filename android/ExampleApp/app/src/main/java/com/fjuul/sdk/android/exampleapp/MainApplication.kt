47db88860167657159970fbaf2037ec58aa42fa8
Dmitry Ovcharov <xesoda@2glab.com>
2021-03-15 13:34:33 +0300

    Fix: remove support of multidex because SDK v2 runs since 21 API level

--- android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/MainApplication.kt
+++ android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/MainApplication.kt
@@ -1,10 +1,10 @@
 package com.fjuul.sdk.android.exampleapp
 
-import androidx.multidex.MultiDexApplication
+import android.app.Application
 import com.fjuul.sdk.core.utils.DebugTimberTree
 import timber.log.Timber
 
-public class MainApplication : MultiDexApplication() {
+public class MainApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {

92fb9a294759c8456588dcfa275524a0e3f1705b
Marina Gudym <mari23rus@gmail.com>
2021-08-13 12:05:41 +0300

    wip: fix observable

--- ios/ExampleApp/ExampleApp/Models/AggregatedDailyStatsObservable.swift
+++ ios/ExampleApp/ExampleApp/Models/AggregatedDailyStatsObservable.swift
@@ -10,24 +10,22 @@ class AggregatedDailyStatsObservable: ObservableObject {
     @Published var fromDate: Date = Date()
     @Published var toDate: Date = Date()
     @Published var aggregation: AggregationType = .sum
-    @Published var value: AggregatedDailyStats 
+    @Published var value: AggregatedDailyStats?
 
     private var dateObserver: AnyCancellable?
 
     init() {
-        dateObserver = $fromDate.combineLatest($toDate).sink { (fromDate, toDate) in
-            self.fetch(fromDate, toDate)
+        dateObserver = $fromDate.combineLatest($toDate, $aggregation).sink { (fromDate, toDate, aggregation) in
+            self.fetch(fromDate, toDate, aggregation)
         }
     }
 
-    func fetch(_ fromDate: Date, _ toDate: Date, _ aggregation: aggregation) {
-        self.value = undefined
+    func fetch(_ fromDate: Date, _ toDate: Date, _ aggregation: AggregationType) {
         self.isLoading = true
         ApiClientHolder.default.apiClient?.analytics.dailyStatsAggregate(from: fromDate, to: toDate, aggregation: aggregation) { result in
             self.isLoading = false
             switch result {
-            case .success(let aggregatedDailyStats):
-                self.value = dailyStats.sorted(by: { $0.date.compare($1.date) == .orderedDescending })
+            case .success(let aggregatedDailyStats): self.value = aggregatedDailyStats
             case .failure(let err): self.error = ErrorHolder(error: err)
             }
         }

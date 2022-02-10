0e54a98766145b3d12bb61cb40398e7fc089159e
prOttonicFusion <61689180+prOttonicFusion@users.noreply.github.com>
2022-02-10 14:17:34 +0200

    update example app UI

--- android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/aggregated_daily_stats/AggregatedDailyStatsFragment.kt
+++ android/ExampleApp/app/src/main/java/com/fjuul/sdk/android/exampleapp/ui/aggregated_daily_stats/AggregatedDailyStatsFragment.kt
@@ -70,7 +70,10 @@ class AggregatedDailyStatsFragment : Fragment() {
                     """
  low: ${it.low.metMinutes} metMinutes;
  moderate: ${it.moderate.metMinutes} metMinutes;
- high: ${it.high.metMinutes} metMinutes"""
+ high: ${it.high.metMinutes} metMinutes;
+ activeKcal: ${it.activeKcal} kcal;
+ bmr: ${it.bmr};
+ steps: ${it.steps}"""
             }
         )
 

e85b08a9d722f1e4f1a2cc50d6cfed7ef3ac339f
Leo Schweizer <leo@fjuul.com>
2023-09-22 15:54:55 +0200

    remove unrelated ActivitySourcesManagerConfig change

--- android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/ActivitySourcesManagerConfig.java
+++ android/activitysources/src/main/java/com/fjuul/sdk/activitysources/entities/ActivitySourcesManagerConfig.java
@@ -288,22 +288,28 @@ public class ActivitySourcesManagerConfig {
     }
 
     /**
-     * Build the default config with the minimum required fitness metrics for core functionality and enabled background
-     * synchronization for intraday and user profile data from Google Fit.
+     * Build the default config with all collectable fitness metrics and enabled background synchronization for intraday
+     * and session data from Google Fit (minimum duration of sessions is 5 minutes), user profile properties.
      *
      * @return config
      */
     @SuppressLint("NewApi")
     @NonNull
     public static ActivitySourcesManagerConfig buildDefault() {
-        final Set<FitnessMetricsType> fitnessMetrics =
-            Stream.of(FitnessMetricsType.INTRADAY_CALORIES, FitnessMetricsType.HEIGHT, FitnessMetricsType.WEIGHT)
+        final Duration minSessionDuration = Duration.ofMinutes(5);
+        final Set<FitnessMetricsType> allFitnessMetrics =
+            Stream
+                .of(FitnessMetricsType.INTRADAY_CALORIES,
+                    FitnessMetricsType.INTRADAY_HEART_RATE,
+                    FitnessMetricsType.INTRADAY_STEPS,
+                    FitnessMetricsType.WORKOUTS,
+                    FitnessMetricsType.HEIGHT,
+                    FitnessMetricsType.WEIGHT)
                 .collect(Collectors.toSet());
         final ActivitySourcesManagerConfig config =
-            new ActivitySourcesManagerConfig.Builder().enableGoogleFitIntradayBackgroundSync()
-                .disableGoogleFitSessionsBackgroundSync()
+            new ActivitySourcesManagerConfig.Builder().enableGoogleFitBackgroundSync(minSessionDuration)
                 .enableProfileBackgroundSync()
-                .setCollectableFitnessMetrics(fitnessMetrics)
+                .setCollectableFitnessMetrics(allFitnessMetrics)
                 .build();
         return config;
     }
